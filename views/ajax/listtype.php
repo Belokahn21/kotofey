caf1bbc7dae10b80dbd7c06e70af8ef4322372d1
small_jedi <web-plast@yandex.ru>
2019-09-27 06:34:53 +0300

    update 27.09.2019

---
+++ views/ajax/listtype.php
@@ -0,0 +1,3 @@
+<?php foreach ($items as $item): ?>
+    <option><?php echo $item->id; ?></option>
+<?php endforeach; ?>
--- widgets/catalog_filter/views/default.php
+++ widgets/catalog_filter/views/default.php
@@ -5,14 +5,16 @@ use app\models\forms\CatalogFilter;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\widgets\Pjax;
 
 /* @var $filterModel CatalogFilter */
 /* @var $listCompany InformersValues[] */
 /* @var $listType InformersValues[] */
 
 ?>
-
 <div class="filter">
+    <?php // Pjax::begin(); ?>
+
     <?php $form = ActiveForm::begin(['options' => ['data-pjax' => true]]); ?>
     <div style="display: flex; flex-direction: row;">
         <?php echo $form->field($filterModel, 'price_from'); ?>
@@ -22,10 +24,17 @@ use yii\widgets\ActiveForm;
         <?php echo $form->field($filterModel, 'weight_from'); ?>
         <?php echo $form->field($filterModel, 'weight_to'); ?>
     </div>
-    <?php echo $form->field($filterModel, 'company')->dropDownList(ArrayHelper::map($listCompany, 'id', 'value'), ['prompt' => 'Производитель']); ?>
-    <?php echo $form->field($filterModel, 'type')->dropDownList(ArrayHelper::map($listType, 'id', 'value'), ['prompt' => 'Тип корма']); ?>
+    <?php echo $form->field($filterModel, 'company')->dropDownList(ArrayHelper::map($listCompany, 'id', 'value'), [
+        'prompt' => 'Производитель',
+        'onchange' => '$.post("/ajax/loadtype/", {company:$(this).val()}, function(data){
+                console.log($("select#id_list_type").html(data));
+            },"HTML");'
+    ]); ?>
+    <?php echo $form->field($filterModel, 'type')->dropDownList(ArrayHelper::map($listType, 'id', 'value'),
+        ['prompt' => 'Тип корма', 'id' => 'id_list_type']); ?>
 
     <?= Html::submitButton('Фильтр', ['class' => 'btn-main']); ?>
     <?php ActiveForm::end() ?>
 
+    <?php // Pjax::end(); ?>
 </div>
