c4c610a888e5e6cdbdbf4e95e522dbdbb68de5cc
Konstantin <popugau@gmail.com>
2019-10-27 20:08:44 +0700

    fix js

--- web/js/site.js
+++ web/js/site.js
@@ -149,9 +149,9 @@ $(document).ready(function () {
         $('.checkout-form').hide().eq($this.index()).show();
     });
 
-    $('.show-catalog-filter').click(function (e) {
-        $('.filter').toggle();
-    });
+    // $('.show-catalog-filter').click(function (e) {
+    //     $('.filter').toggle();
+    // });
 
     $("#filter-form-id").catalogFilter();
 });
