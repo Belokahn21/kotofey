ad82d0c3a87fee4cdd46e30aed2d70cbbc746054
Konstantin <popugau@gmail.com>
2019-10-28 18:31:01 +0700

    fix db sonsole

--- config/db.php
+++ config/db.php
@@ -1,30 +1,30 @@
 <?php
 
 
-if (YII_ENV == 'prod') {
-    return [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=cd91333_kotofey',
-        'username' => 'cd91333_kotofey',
-        'password' => '123qweR%',
-        'charset' => 'utf8',
+if (YII_ENV == 'prod' or empty($_SERVER['DOCUMENT_ROOT'])) {
+	return [
+		'class' => 'yii\db\Connection',
+		'dsn' => 'mysql:host=localhost;dbname=cd91333_kotofey',
+		'username' => 'cd91333_kotofey',
+		'password' => '123qweR%',
+		'charset' => 'utf8',
 
-        // Schema cache options (for production environment)
-        'enableSchemaCache' => true,
-        'schemaCacheDuration' => 60,
-        'schemaCache' => 'cache',
-    ];
+		// Schema cache options (for production environment)
+		'enableSchemaCache' => true,
+		'schemaCacheDuration' => 60,
+		'schemaCache' => 'cache',
+	];
 } else {
-    return [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=kotofey_shop',
-        'username' => 'root',
-        'password' => '',
-        'charset' => 'utf8',
+	return [
+		'class' => 'yii\db\Connection',
+		'dsn' => 'mysql:host=localhost;dbname=kotofey_shop',
+		'username' => 'root',
+		'password' => '',
+		'charset' => 'utf8',
 
-        // Schema cache options (for production environment)
-        'enableSchemaCache' => true,
-        'schemaCacheDuration' => 60,
-        'schemaCache' => 'cache',
-    ];
+		// Schema cache options (for production environment)
+		'enableSchemaCache' => true,
+		'schemaCacheDuration' => 60,
+		'schemaCache' => 'cache',
+	];
 }
