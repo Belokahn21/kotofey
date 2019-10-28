4a8a1352d8d41e5ffc059233b224e6b6923c1d75
Konstantin <popugau@gmail.com>
2019-10-28 16:02:29 +0700

    fix css

---
+++ views/site/_type-order.php
@@ -0,0 +1,21 @@
+<div class="type-order">
+	<ul class="type-order-list">
+		<li class="type-order-list__item" data-cookie="fast">
+			<div class="type-order-list__item-title">Быстрый заказ</div>
+			<div class="type-order-list__item-reason">
+				<ul class="list-advantages">
+					<li class="advantage-item advantage-false">Нет бонусов</li>
+				</ul>
+			</div>
+		</li>
+		<li class="type-order-list__item" data-cookie="normal">
+			<div class="type-order-list__item-title">Обычный заказ</div>
+			<div class="type-order-list__item-reason">
+				<ul class="list-advantages">
+					<li class="advantage-item advantage-true">Вам начислят бонусы</li>
+					<li class="advantage-item advantage-true">Можно использовать промокод</li>
+				</ul>
+			</div>
+		</li>
+	</ul>
+</div>
--- views/site/checkout_user.php
+++ views/site/checkout_user.php
@@ -21,28 +21,7 @@ $this->params['breadcrumbs'][] = ['label' => 'Оформление заказа'
 ?>
 <section class="checkout">
     <div class="checkout-form-wrap">
-        <div class="type-order">
-            <ul class="type-order-list">
-                <li class="type-order-list__item" data-cookie="fast">
-                    <div class="type-order-list__item-title">Быстрый заказ</div>
-                    <div class="type-order-list__item-reason">
-                        <ul class="list-advantages">
-                            <li class="advantage-item advantage-false">Нет бонусов</li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="type-order-list__item" data-cookie="normal">
-                    <div class="type-order-list__item-title">Обычный заказ</div>
-                    <div class="type-order-list__item-reason">
-                        <ul class="list-advantages">
-                            <li class="advantage-item advantage-true">Вам начислят бонусы</li>
-                            <li class="advantage-item advantage-true">Можно использовать промокод</li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </div>
-
+        <?= $this->render('_type-order'); ?>
         <div class="checkout-form fast">
             <div class="left-col">
                 <h2>Быстрый заказ</h2>
