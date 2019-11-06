4fb431ea9880c0d053afcd1c6b62c879e91f812c
Konstantin <popugau@gmail.com>
2019-11-06 16:10:30 +0700

    fix db

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
-//if (YII_ENV == 'prod' or empty($_SERVER['DOCUMENT_ROOT'])) {
-if (YII_ENV == 'prod') {
+if (YII_ENV == 'prod' or empty($_SERVER['DOCUMENT_ROOT'])) {
+//if (YII_ENV == 'prod') {
 	return [
 		'class' => 'yii\db\Connection',
 		'dsn' => 'mysql:host=localhost;dbname=cd91333_kotofey',
