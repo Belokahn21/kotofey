03067783552d5503272083eb4221c5f5f02a34d4
Konstantin <popugau@gmail.com>
2019-11-10 20:09:04 +0700

    fix

--- views/ajax/basket.php
+++ views/ajax/basket.php
@@ -3,6 +3,6 @@
 use app\models\entity\Basket;
 
 ?>
-<?= Yii::$app->i18n->format("{n, plural, =0{Корзина пуста} =1{В крзине # товар} one{В крзине # товар} few{В корзине # товаров} many{В корзине # товаров} other{Корзина пуста}}", ['n' => Basket::count()], 'ru_RU'); ?>
+<?= Yii::$app->i18n->format("{n, plural, =0{Корзина пуста} =1{В крзине # товар} one{В крзине # товар} few{В корзине # товара} many{В корзине # товаров} other{Корзина пуста}}", ['n' => Basket::count()], 'ru_RU'); ?>
 
 
