f568104361f249e485be9c66d130a6ed4b315e94
Konstantin <popugau@gmail.com>
2019-11-17 21:47:41 +0700

    update

--- views/admin/_forms/_catalog.php
+++ views/admin/_forms/_catalog.php
@@ -19,84 +19,83 @@ use yii\helpers\Json;
     </ul>
 
     <div id="tab-1" class="tab-content current">
-		<?= $form->field($model, 'name'); ?>
-		<?= $form->field($model, 'description')->textarea(); ?>
+        <?= $form->field($model, 'name'); ?>
+        <?= $form->field($model, 'description')->textarea(); ?>
         <div class="product-form__price">
-			<?= $form->field($model, 'price')->textInput(['id' => 'id-price']); ?>
+            <?= $form->field($model, 'price')->textInput(['id' => 'id-price']); ?>
             <div class="set-price">
                 <a href="" onclick="document.getElementById('id-price').value=(parseInt(document.getElementById('id-purchase').value)+parseInt((document.getElementById('id-purchase').value*<?= SiteSettings::getValueByCode('saleup') ?>)/100)); return false;">% наценка</a>
             </div>
         </div>
         <div class="product-form__purchase">
-			<?= $form->field($model, 'purchase')->textInput(['id' => 'id-purchase']); ?>
+            <?= $form->field($model, 'purchase')->textInput(['id' => 'id-purchase']); ?>
         </div>
         <div class="product-form__count">
-			<?= $form->field($model, 'count'); ?>
+            <?= $form->field($model, 'count'); ?>
         </div>
         <div class="clearfix"></div>
-		<?= $form->field($model, 'category')->dropDownList(ArrayHelper::map((new Category())->categoryTree(), 'id', 'name'), ['prompt' => 'Раздел']); ?>
+        <?= $form->field($model, 'category')->dropDownList(ArrayHelper::map((new Category())->categoryTree(), 'id', 'name'), ['prompt' => 'Раздел']); ?>
         <div class="clearfix"></div>
-        <hr/>
+        <?= $form->field($model, 'has_store')->checkbox(); ?>
     </div>
     <div id="tab-2" class="tab-content">
-		<?= $form->field($model, 'seo_description')->textarea(); ?>
-		<?= $form->field($model, 'seo_keywords'); ?>
+        <?= $form->field($model, 'seo_description')->textarea(); ?>
+        <?= $form->field($model, 'seo_keywords'); ?>
     </div>
     <div id="tab-3" class="tab-content">
         <div class="product-form__simple-image">
-			<?php if ($model->image): ?>
+            <?php if ($model->image): ?>
                 <img src="/web/upload/<?= $model->image; ?>" width="200">
-			<?php endif; ?>
-			<?= $form->field($model, 'image')->fileInput(); ?>
+            <?php endif; ?>
+            <?= $form->field($model, 'image')->fileInput(); ?>
         </div>
         <div class="product-form__more-image">
-			<?php if ($model->images): ?>
-				<?php foreach (Json::decode($model->images) as $image): ?>
+            <?php if ($model->images): ?>
+                <?php foreach (Json::decode($model->images) as $image): ?>
                     <img src="/web/upload/<?= $image; ?>" width="200">
-				<?php endforeach; ?>
-			<?php endif; ?>
-			<?= $form->field($model, 'imagesFiles[]')->fileInput([
-				'multiple' => true,
-				'accept' => 'image/*'
-			]); ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <?= $form->field($model, 'imagesFiles[]')->fileInput([
+                'multiple' => true,
+                'accept' => 'image/*'
+            ]); ?>
         </div>
         <div class="clearfix"></div>
     </div>
     <div id="tab-4" class="tab-content">
         <div class="left-col">
-			<?= $form->field($model, 'code')->textInput(); ?>
-			<?= $form->field($model, 'vitrine')->radioList(["Нет", "Да"]); ?>
-			<?= $form->field($model, 'active')->radioList(["Не активен", "Активен"]); ?>
-			<?= $form->field($model, 'stock_id')->dropDownList(ArrayHelper::map(Stocks::find()->all(), 'id', 'name')) ?>
+            <?= $form->field($model, 'code')->textInput(); ?>
+            <?= $form->field($model, 'vitrine')->radioList(["Нет", "Да"]); ?>
+            <?= $form->field($model, 'active')->radioList(["Не активен", "Активен"]); ?>
+            <?= $form->field($model, 'stock_id')->dropDownList(ArrayHelper::map(Stocks::find()->all(), 'id', 'name')) ?>
         </div>
         <div class="right-col">
-			<?= $form->field($model, 'has_store')->checkbox(); ?>
         </div>
         <div class="clearfix"></div>
     </div>
     <div id="tab-5" class="tab-content">
         <ul style="list-style: none; margin: 0; padding: 0;">
-			<? /* @var $property \app\models\entity\ProductProperties */ ?>
-			<?php try { ?>
+            <? /* @var $property \app\models\entity\ProductProperties */ ?>
+            <?php try { ?>
                 <ul style="list-style: none; margin: 0; padding: 0;">
-					<? foreach ($properties as $property): ?>
-						<? if ($property->type == 1): ?>
-							<?php $value = ProductPropertiesValues::findOne([
-								'product_id' => $model->id,
-								'property_id' => $property->id
-							]);
-							if ($value):
-								$model->properties[$property->id] = $value->value;
-							endif; ?>
-							<?= $form->field($model, 'properties[' . $property->id . ']')->dropDownList(ArrayHelper::map(InformersValues::find()->where(['informer_id' => $property->informer_id])->all(), 'id', 'name'), ['prompt' => $property->name])->label($property->name); ?>
-						<? else: ?>
-							<?= $form->field($model, 'properties[' . $property->id . ']')->textInput(['value' => ProductPropertiesValues::findOne(['product_id' => $model->id, 'property_id' => $property->id])->value])->label($property->name); ?>
-						<? endif; ?>
-					<? endforeach; ?>
+                    <? foreach ($properties as $property): ?>
+                        <? if ($property->type == 1): ?>
+                            <?php $value = ProductPropertiesValues::findOne([
+                                'product_id' => $model->id,
+                                'property_id' => $property->id
+                            ]);
+                            if ($value):
+                                $model->properties[$property->id] = $value->value;
+                            endif; ?>
+                            <?= $form->field($model, 'properties[' . $property->id . ']')->dropDownList(ArrayHelper::map(InformersValues::find()->where(['informer_id' => $property->informer_id])->all(), 'id', 'name'), ['prompt' => $property->name])->label($property->name); ?>
+                        <? else: ?>
+                            <?= $form->field($model, 'properties[' . $property->id . ']')->textInput(['value' => ProductPropertiesValues::findOne(['product_id' => $model->id, 'property_id' => $property->id])->value])->label($property->name); ?>
+                        <? endif; ?>
+                    <? endforeach; ?>
                 </ul>
-			<?php } catch (ErrorException $exception) { ?>
-				<?= $exception->getMessage(); ?>
-			<?php } ?>
+            <?php } catch (ErrorException $exception) { ?>
+                <?= $exception->getMessage(); ?>
+            <?php } ?>
         </ul>
     </div>
 </div>
--- views/site/test.php
+++ views/site/test.php
@@ -1,16 +1,76 @@
 <?php
 
 
-use app\models\tool\vk\Market;
-use app\models\tool\vk\MarketProduct;
+use VK\Client\VKApiClient;
+use VK\OAuth\Scopes\VKOAuthUserScope;
+use VK\OAuth\VKOAuth;
+use VK\OAuth\VKOAuthDisplay;
+use VK\OAuth\VKOAuthResponseType;
 
-$product = new MarketProduct();
-$product->name = 'Товар для ВК';
-$product->description = 'Описание товара';
-$product->price = 800;
+$oauth = new VKOAuth();
+$client_id = 7209302;
+$redirect_uri = 'http://local.shop-kotofey.ru';
+$display = VKOAuthDisplay::PAGE;
+$scope = array(VKOAuthUserScope::WALL, VKOAuthUserScope::GROUPS, VKOAuthUserScope::MARKET, VKOAuthUserScope::PHOTOS);
+$state = 'secret_state_code';
+$revoke_auth = true;
+
+$browser_url = $oauth->getAuthorizeUrl(VKOAuthResponseType::TOKEN, $client_id, $redirect_uri, $display, $scope, $state, null, $revoke_auth);
 
 
-$market = new Market();
-$response = $market->add($product);
+echo \yii\helpers\Html::a('click', $browser_url);
 
-echo "Товар был добавлен";
+$access_token = '445c6e32c40949c9ebae2b1dd9f193f823ba7128320b5644b4baf85109a9b4f52df298910e9689225d29d';
+$vk = new VKApiClient();
+if ($access_token) {
+    $response = $vk->photos()->getMarketUploadServer($access_token, [
+        'group_id' => 185683081,
+        'main_photo' => 1,
+    ]);
+    if ($curl = curl_init()) {
+
+        $filename = realpath('G:\projects\local.shop-kotofey.ru\views\site\product.png');
+        $finfo = new \finfo(FILEINFO_MIME_TYPE);
+        $mimetype = $finfo->file($filename);
+
+
+        curl_setopt($curl, CURLOPT_URL, $response['upload_url']);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: multipart/form-data"));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, [
+            'file' => curl_file_create($filename, $mimetype, basename($filename))
+        ]);
+        $out = curl_exec($curl);
+        $obj = \yii\helpers\Json::decode($out);
+        curl_close($curl);
+
+
+        $answer = $vk->photos()->saveMarketPhoto($access_token, [
+            'group_id' => 185683081,
+            'photo' => $obj['photo'],
+            'server' => $obj['server'],
+            'hash' => $obj['hash'],
+            'crop_data' => $obj['crop_data'],
+            'crop_hash' => $obj['crop_hash'],
+        ]);
+
+
+//        \app\models\tool\Debug::p($answer);
+
+
+        $response = $vk->market()->add($access_token, [
+            'owner_id' => -185683081,
+            'name' => 'test',
+            'description' => 'test description',
+            'category_id' => '1',
+            'main_photo_id' => $answer[0]['id'],
+            'price' => '100500',
+            'url' => 'https://kotofey.store/test/'
+        ]);
+
+        \app\models\tool\Debug::p($response);
+
+    }
+}
+
