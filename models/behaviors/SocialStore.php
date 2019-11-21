6ef67c55b212cbf43f989f6962a3c44d28d61a71
Konstantin <popugau@gmail.com>
2019-11-21 18:06:30 +0700

    fi

--- models/behaviors/SocialStore.php
+++ models/behaviors/SocialStore.php
@@ -79,7 +79,7 @@ class SocialStore extends Behavior
 							'owner_id' => -$group_id,
 							'name' => $model->name,
 							'description' => $model->description,
-							'category_id' => '1',
+							'category_id' => '10',
 							'main_photo_id' => $answer[0]['id'],
 							'price' => $model->price,
 							'url' => 'https://kotofey.store/test/'
--- views/site/test.php
+++ views/site/test.php
@@ -18,9 +18,9 @@ $browser_url = $oauth->getAuthorizeUrl(VKOAuthResponseType::TOKEN, $client_id, $
 echo \yii\helpers\Html::a('click', $browser_url);
 
 $access_token = '9a59bee577bfac9297aaab387a5d22ca36b847e5b414823b33f7c53f0e214d4ee828f1e8755004c99c515';
-$vk = new VKApiClient();
+$vk = new \VK\Client\VKApiClient();
 if ($access_token) {
-	\app\models\tool\Debug::p($vk->market()->getCategories($access_token, [
-		'count' => 1000
-	]));
+//	\app\models\tool\Debug::p($vk->market()->getCategories($access_token, [
+//		'count' => 1000
+//	]));
 }
