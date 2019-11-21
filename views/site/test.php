d13eacbb8c9ad4b258442b83e02c2903a60b2371
Konstantin <popugau@gmail.com>
2019-11-21 17:43:03 +0700

    fix

--- views/site/test.php
+++ views/site/test.php
@@ -1,7 +1,5 @@
 <?php
 
-
-use VK\Client\VKApiClient;
 use VK\OAuth\Scopes\VKOAuthUserScope;
 use VK\OAuth\VKOAuth;
 use VK\OAuth\VKOAuthDisplay;
@@ -9,7 +7,7 @@ use VK\OAuth\VKOAuthResponseType;
 
 $oauth = new VKOAuth();
 $client_id = 7209302;
-$redirect_uri = 'http://local.shop-kotofey.ru';
+$redirect_uri = 'https://kotofey.store';
 $display = VKOAuthDisplay::PAGE;
 $scope = array(VKOAuthUserScope::WALL, VKOAuthUserScope::GROUPS, VKOAuthUserScope::MARKET, VKOAuthUserScope::PHOTOS, VKOAuthUserScope::OFFLINE);
 $state = 'secret_state_code';
@@ -19,10 +17,10 @@ $browser_url = $oauth->getAuthorizeUrl(VKOAuthResponseType::TOKEN, $client_id, $
 
 echo \yii\helpers\Html::a('click', $browser_url);
 
-$access_token = '445c6e32c40949c9ebae2b1dd9f193f823ba7128320b5644b4baf85109a9b4f52df298910e9689225d29d';
-$vk = new VKApiClient();
-if ($access_token) {
-//	\app\models\tool\Debug::p($vk->market()->getCategories($access_token, [
-//		'count' => 1000
-//	]));
-}
+//$access_token = '445c6e32c40949c9ebae2b1dd9f193f823ba7128320b5644b4baf85109a9b4f52df298910e9689225d29d';
+//$vk = new VKApiClient();
+//if ($access_token) {
+////	\app\models\tool\Debug::p($vk->market()->getCategories($access_token, [
+////		'count' => 1000
+////	]));
+//}
