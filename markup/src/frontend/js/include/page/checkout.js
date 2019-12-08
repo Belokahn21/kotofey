b7134b18b2fef5663f6b0a0d0d4cff868c88e718
Konstantin <popugau@gmail.com>
2019-12-08 15:24:40 +0700

    fix

--- markup/src/frontend/js/include/page/checkout.js
+++ markup/src/frontend/js/include/page/checkout.js
@@ -6,4 +6,16 @@ $(document).ready(function () {
         $('.order-type-form-wrap').find('.order-type-form').css('display', 'none');
         $('.order-type-form-wrap').find('.order-type-form.' + type).css('display', 'flex');
     });
+
+
+    $('.type-order__item').click(function (e) {
+        var target = $(this).data('checkout');
+
+
+        $('.checkout-order').addClass('hide');
+        $('.checkout-order[data-type=' + target + ']').removeClass('hide')
+
+    });
+
+
 });
