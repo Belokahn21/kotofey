8ac148beb0c706186bf2cf156df0a3ecc7141bdd
Konstantin <popugau@gmail.com>
2019-12-19 16:59:34 +0700

    fix

--- models/entity/Compare.php
+++ models/entity/Compare.php
@@ -16,6 +16,7 @@ class Compare
 			return false;
 		}
 
+		\Yii::$app->session->open();
 		$_SESSION[self::COMPARE_SESSION_KEY][$product->id] = $product;
 
 		if (array_key_exists(self::COMPARE_SESSION_KEY, $_SESSION)) {
@@ -30,6 +31,7 @@ class Compare
 	public static function findAll()
 	{
 		$items = array();
+		\Yii::$app->session->open();
 		if ($compare = \Yii::$app->session->get(self::COMPARE_SESSION_KEY)) {
 			foreach ($compare as $product_id => $item) {
 				$items[] = Product::findOne($product_id);
