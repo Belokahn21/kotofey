807292dfeb6db1f83402651e5fbee824797a6655
mijeko <123qweR%1>
2019-12-29 13:36:33 +0700

    fix

--- markup/src/backend/js/script.js
+++ markup/src/backend/js/script.js
@@ -4,26 +4,38 @@
 
 
 $(document).ready(function () {
-	/* Скрытие placeholder элемента */
-	var placeholder = "";
-	$("input[type=text], textarea").click(function () {
-		placeholder = $(this).attr('placeholder');
-		$(this).attr('placeholder', "");
-	}).blur(function () {
-		$(this).attr('placeholder', placeholder);
-		placeholder = "";
-	});
+    /* Скрытие placeholder элемента */
+    var placeholder = "";
+    $("input[type=text], textarea").click(function () {
+        placeholder = $(this).attr('placeholder');
+        $(this).attr('placeholder', "");
+    }).blur(function () {
+        $(this).attr('placeholder', placeholder);
+        placeholder = "";
+    });
 
-	/* Показывать модалку если она есть -- старт */
-	if ($('#update-task').length > 0) {
-		$('#update-task').modal('show');
-	}
-	/* Показывать модалку если она есть -- конец */
+    /* Показывать модалку если она есть -- старт */
+    if ($('#update-task').length > 0) {
+        $('#update-task').modal('show');
+    }
+    /* Показывать модалку если она есть -- конец */
 
-	/* Запуск своих плагинов -- старт */
-	$('.pre-load-catalog').catalogLoader();
-	/* Запуск своих плагинов -- конец */
+    /* Запуск своих плагинов -- старт */
+    $('.pre-load-catalog').catalogLoader();
+    /* Запуск своих плагинов -- конец */
 
-	$('[data-toggle="tooltip"]').tooltip();
+    /* закрыть уведомление -- начало */
+    $('.notify__close').click(function () {
+        $('.notify').remove();
+    });
+    /* закрыть уведомление -- конец */
+
+    /* поаказать меню -- начало */
+    $('.switch-menu').click(function () {
+        $('.dashboard-left-sidebar').toggle();
+    });
+    /* поаказать меню -- конец */
+
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
