6023f3c6e64a6bd8638fc65bce12e39dff604738
mijeko <123qweR%1>
2020-01-04 14:12:39 +0700

    fix

--- controllers/YandexController.php
+++ controllers/YandexController.php
@@ -12,7 +12,18 @@ class YandexController extends Controller
 
     public function actionExport()
     {
-        \Yii::$app->response->format = \Yii\web\Response::FORMAT_XML;
-        $this->renderPartial('export');
+//        \Yii::$app->response->format = \yii\web\Response::FORMAT_XML;
+
+        $xml = new \SimpleXMLElement('<xml/>');
+
+        for ($i = 1; $i <= 8; ++$i) {
+            $track = $xml->addChild('track');
+            $track->addChild('path', "song$i.mp3");
+            $track->addChild('title', "Track $i - Track Title");
+        }
+
+
+        Header('Content-type: text/xml');
+        return $xml->asXML();
     }
 }
