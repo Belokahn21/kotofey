dbc4f641b0c9a8d7477729aec84c7b242e6efd75
mijeko <123qweR%1>
2020-01-04 14:14:40 +0700

    fix

--- controllers/YandexController.php
+++ controllers/YandexController.php
@@ -14,16 +14,59 @@ class YandexController extends Controller
     {
 //        \Yii::$app->response->format = \yii\web\Response::FORMAT_XML;
 
-        $xml = new \SimpleXMLElement('<xml/>');
 
-        for ($i = 1; $i <= 8; ++$i) {
-            $track = $xml->addChild('track');
-            $track->addChild('path', "song$i.mp3");
-            $track->addChild('title', "Track $i - Track Title");
+        $dom = new \DOMDocument("1.0", "UTF-8");
+        $yml_catalog = $dom->createElement('yml_catalog');
+        $shop = $dom->createElement('shop');
+        $offers = $dom->createElement('offers');
+        $dom->appendChild($yml_catalog);
+
+        $yml_catalog->setAttribute('date', date("Y-m-d H:i"));
+        $yml_catalog->appendChild($shop);
+
+        $shop->appendChild($offers);
+        /* @var $product Product */
+        foreach (Product::find()->all() as $product) {
+
+            $offer = $dom->createElement('offer');
+            $offer->setAttribute('id', $product->id);
+
+            $name = $dom->createElement('name', htmlspecialchars($product->display));
+            $offer->appendChild($name);
+
+            $currencyId = $dom->createElement('currencyId', "RUB");
+            $offer->appendChild($currencyId);
+
+            $url = $dom->createElement('url', sprintf("https://kotofey.store/%s", $product->detail));
+            $offer->appendChild($url);
+
+            $price = $dom->createElement('price', $product->price);
+            $offer->appendChild($price);
+
+            $picture = $dom->createElement('picture', sprintf("https://kotofey.store/%s", $product->image));
+            $offer->appendChild($picture);
+
+            $categoryId = $dom->createElement('categoryId', $product->category_id);
+            $offer->appendChild($categoryId);
+
+            if (!empty($product->description)) {
+
+                $description = $dom->createElement('description', htmlspecialchars($product->description));
+                $offer->appendChild($description);
+
+            }
+
+            $delivery = $dom->createElement('delivery', "true");
+            $offer->appendChild($delivery);
+
+            $pickup = $dom->createElement('pickup', "true");
+            $offer->appendChild($pickup);
+
+            $offers->appendChild($offer);
         }
 
 
-        Header('Content-type: text/xml');
-        return $xml->asXML();
+
+        return $dom->saveXML();
     }
 }
