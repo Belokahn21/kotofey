b591c8fe667726eecf7c57dabb4030938d400a0b
mijeko <123qweR%1>
2020-02-05 23:13:30 +0700

    fix

--- widgets/slider/views/default.php
+++ widgets/slider/views/default.php
@@ -7,30 +7,32 @@ use yii\helpers\Html;
 
 ?>
 <?php if ($images): ?>
-    <div id="main-page-slider-id" class="carousel slide main-page-slider" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#main-page-slider-id" data-slide-to="0" class="active"></li>
-            <li data-target="#main-page-slider-id" data-slide-to="1"></li>
-            <li data-target="#main-page-slider-id" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <?php $iterator = 1; ?>
-            <?php foreach ($images as $image): ?>
-                <div class="carousel-item main-page-slider__item <?= ($iterator == 1 ? 'active' : ''); ?>">
-                    <a href="<?= (!empty($image->link) ? $image->link : 'javascript:void(0);'); ?>">
-                        <img class="d-block w-100 h-100" src="/web/upload/<?= $image->image; ?>" alt="<?= $image->text; ?>">
-                    </a>
-                </div>
-                <?php $iterator++; ?>
-            <?php endforeach; ?>
+    <?php if ($this->beginCache('index-slider', ['duration' => 3600 * 24 * 7])): ?>
+        <div id="main-page-slider-id" class="carousel slide main-page-slider" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#main-page-slider-id" data-slide-to="0" class="active"></li>
+                <li data-target="#main-page-slider-id" data-slide-to="1"></li>
+                <li data-target="#main-page-slider-id" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <?php $iterator = 1; ?>
+                <?php foreach ($images as $image): ?>
+                    <div class="carousel-item main-page-slider__item <?= ($iterator == 1 ? 'active' : ''); ?>">
+                        <a href="<?= (!empty($image->link) ? $image->link : 'javascript:void(0);'); ?>">
+                            <img class="d-block w-100 h-100" src="/web/upload/<?= $image->image; ?>" alt="<?= $image->text; ?>">
+                        </a>
+                    </div>
+                    <?php $iterator++; ?>
+                <?php endforeach; ?>
+            </div>
+            <a class="carousel-control-prev" href="#main-page-slider-id" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#main-page-slider-id" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <a class="carousel-control-prev" href="#main-page-slider-id" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#main-page-slider-id" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+        <?php $this->endCache(); endif; ?>
 <?php endif; ?>
