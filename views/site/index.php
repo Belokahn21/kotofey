119e5452f0a6e8b1df51f1990158535e0e17c1bc
Konstantin <popugau@gmail.com>
2020-02-07 12:17:56 +0700

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -25,7 +25,7 @@ $this->title = Title::showTitle("Зоотовары для животных в �
 				<?php foreach ($providers as $provider): ?>
                     <li class="homepage-providers__item">
                         <a class="homepage-providers__link" href="<?= $provider->link; ?>">
-                            <img class="homepage-providers__image" src="/web/upload/<?= $provider->image; ?>" title="<?= $provider->name; ?>" alt="<?= $provider->name; ?>">
+                            <img class="homepage-providers__image" src="/upload/<?= $provider->image; ?>" title="<?= $provider->name; ?>" alt="<?= $provider->name; ?>">
                             <div class="homepage-providers__detail">К ассортимену</div>
                         </a>
                     </li>
