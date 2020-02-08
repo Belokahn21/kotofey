6c54b681aea2dd06747806d9874dd7e8020d53d9
mijeko <123qweR%1>
2020-02-08 22:20:52 +0700

    fix

--- views/admin/catalog.php
+++ views/admin/catalog.php
@@ -20,37 +20,6 @@ use app\models\entity\SiteSettings;
 $this->title = Title::showTitle("Товары"); ?>
     <section>
         <h1 class="title">Товары</h1>
-        <div class="catalog-control">
-            <div class="pre-load-catalog-wrap">
-                <input type="text" name="url" placeholder="Ссылка на подгрузку" class="pre-load-catalog">
-                <svg width="20" height="30" class="backend-preloader hide">
-                    <rect width="10%" x="5%" rx="5%">
-                        <animate attributeName="height" values="20%; 70%; 20%" dur="0.7s" repeatCount="indefinite"></animate>
-                        <animate attributeName="y" values="40%; 15%; 40%" dur="0.7s" repeatCount="indefinite"></animate>
-                    </rect>
-
-                    <rect width="10%" x="25%" rx="5%">
-                        <animate attributeName="height" values="20%; 70%; 20%" dur="0.7s" begin="0.15s" repeatCount="indefinite"></animate>
-                        <animate attributeName="y" values="40%; 15%; 40%" dur="0.7s" begin="0.15s" repeatCount="indefinite"></animate>
-                    </rect>
-
-                    <rect width="10%" x="45%" rx="5%">
-                        <animate attributeName="height" values="20%; 70%; 20%" dur="0.7s" begin="0.3s" repeatCount="indefinite"></animate>
-                        <animate attributeName="y" values="40%; 15%; 40%" dur="0.7s" begin="0.3s" repeatCount="indefinite"></animate>
-                    </rect>
-
-                    <rect width="10%" x="65%" rx="5%">
-                        <animate attributeName="height" values="20%; 70%; 20%" dur="0.7s" begin="0.45s" repeatCount="indefinite"></animate>
-                        <animate attributeName="y" values="40%; 15%; 40%" dur="0.7s" begin="0.45s" repeatCount="indefinite"></animate>
-                    </rect>
-
-                    <rect width="10%" x="85%" rx="5%">
-                        <animate attributeName="height" values="20%; 70%; 20%" dur="0.7s" begin="0.6s" repeatCount="indefinite"></animate>
-                        <animate attributeName="y" values="40%; 15%; 40%" dur="0.7s" begin="0.6s" repeatCount="indefinite"></animate>
-                    </rect>
-                </svg>
-            </div>
-        </div>
         <?php $form = ActiveForm::begin([
             'enableAjaxValidation' => true,
             'options' => ['enctype' => 'multipart/form-data']
