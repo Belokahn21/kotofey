d4e4fc8c7267477be989f21d55dc40c122d4fc86
Konstantin <popugau@gmail.com>
2020-02-20 11:39:20 +0700

    fix

--- views/site/search.php
+++ views/site/search.php
@@ -72,16 +72,16 @@ $this->params['breadcrumbs'][] = ['label' => 'Поиск по сайту', 'url'
 		<?php endforeach; ?>
     </ul>
 <?php else: ?>
-    <p style="text-align: center; margin: 20px 0;">
+    <p style="text-align: center; margin: 20px 0; padding: 0 10px;">
         К сожаление ничего не нашлось :(
     </p>
-    <p style="text-align: center;margin: 20px 0;">
+    <p style="text-align: center;margin: 20px 0; padding: 0 10px;">
         Попробуйте изменить тактику поиска, поменяв язык. К примеру искали <strong>"Проплан"</strong> попробуйте <strong>"Pro plan"</strong>
     </p>
-    <p style="text-align: center;margin: 20px 0;">
+    <p style="text-align: center;margin: 20px 0; padding: 0 10px;">
         Позвоните нам по номеру <a href="tel:<?= SiteSettings::getValueByCode('phone_1'); ?>" class="phone_mask" style="color: #ff1a4a; font-weight: bold;"><?= SiteSettings::getValueByCode('phone_1'); ?></a> и мы быстро найдем ваш товар
     </p>
-    <p style="text-align: center;margin: 20px 0;">
+    <p style="text-align: center;margin: 20px 0; padding: 0 10px;">
         Или воспользуйтесь чатом в правом нижнем углу, мы отвечаем <span style="color: #ff1a4a; font-weight: bold; text-transform: uppercase;">моментально</span>
     </p>
     <img src="/upload/images/chat.png"/ style="width: auto; margin: auto auto;">
