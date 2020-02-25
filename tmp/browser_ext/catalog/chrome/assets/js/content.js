deabd16f9b05d11e56ea30b6d58280ed39a887ee
Konstantin <popugau@gmail.com>
2020-02-25 16:23:11 +0700

    fix

--- tmp/browser_ext/catalog/chrome/assets/js/content.js
+++ tmp/browser_ext/catalog/chrome/assets/js/content.js
@@ -1,10 +1,14 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	const re = new RegExp('bear', 'gi');
+	const re = new RegExp('каталог', 'gi');
 	const matches = document.documentElement.innerHTML.match(re);
 
 	const count = 0;
 	if (matches != null) {
 		count = matches.length;
+	}
+
+	if (count === undefined) {
+		count = 0;
 	}
 
 	sendResponse({count: count});
