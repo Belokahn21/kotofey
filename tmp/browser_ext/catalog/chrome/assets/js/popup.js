b5eb6b54938447c49f198cf05cc3ba88c5076580
mijeko <123qweR%1>
2020-02-25 23:52:52 +0700

    fix

--- tmp/browser_ext/catalog/chrome/assets/js/popup.js
+++ tmp/browser_ext/catalog/chrome/assets/js/popup.js
@@ -1,22 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
-	document.querySelector('button').addEventListener('click', onClick, false);
+    document.querySelector('button').addEventListener('click', onClick, false);
 
-	function onClick() {
-		chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, 'hi !', setCount);
-		});
-	}
+    function onClick() {
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, 'hi !', setCount);
+        });
+    }
 
-	function setCount(res) {
-		const div = document.createElement('div');
-		let html = "";
+    function setCount(res) {
+        const div = document.createElement('div');
+        let html = "";
 
-		for (var i in res) {
-			html += "Название: " + `${res.element.name}` + "<br>";
-		}
+        let form = document.createElement('form');
+        form.setAttribute('class','milenium-form');
 
-		div.innerHTML = html;
-		document.body.appendChild(div);
+        let nameInput = document.createElement('input');
+        nameInput.setAttribute('name', 'name');
+        nameInput.setAttribute('value', res.element.name);
 
-	}
+
+		let priceInput = document.createElement('input');
+		priceInput.setAttribute('name', 'price');
+		priceInput.setAttribute('value', res.element.price);
+
+
+		let articleInput = document.createElement('input');
+		articleInput.setAttribute('name', 'price');
+		articleInput.setAttribute('value', res.element.article);
+
+
+		let weightInput = document.createElement('input');
+		weightInput.setAttribute('name', 'price');
+		weightInput.setAttribute('value', res.element.weight);
+
+        form.appendChild(nameInput);
+        form.appendChild(priceInput);
+        form.appendChild(articleInput);
+        form.appendChild(weightInput);
+
+        document.body.appendChild(form);
+
+    }
 }, false);
