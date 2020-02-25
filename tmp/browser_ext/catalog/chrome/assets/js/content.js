d1f4ae9e25a6d2b7c8a94199821cba2e4974aed3
Konstantin <popugau@gmail.com>
2020-02-25 16:50:22 +0700

    fix

--- tmp/browser_ext/catalog/chrome/assets/js/content.js
+++ tmp/browser_ext/catalog/chrome/assets/js/content.js
@@ -1,15 +1,4 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	const re = new RegExp('каталог', 'gi');
-	const matches = document.documentElement.innerHTML.match(re);
-
-	const count = 0;
-	if (matches != null) {
-		count = matches.length;
-	}
-
-	if (count === undefined) {
-		count = 0;
-	}
-
-	sendResponse({count: count});
+	price = document.querySelector('.lead').textContent.match(new RegExp(/(\d+)р./,'gi'));
+	sendResponse({count: price});
 });
