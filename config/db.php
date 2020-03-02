458ad8f96e6ccd9c91ae1e5003567ca532ffa332
mijeko <123qweR%1>
2020-03-02 21:35:05 +0700

    update

--- config/db.php
+++ config/db.php
@@ -1,29 +1,13 @@
 <?php
-if (YII_ENV == 'prod' and empty($_SERVER['DOCUMENT_ROOT']) or YII_ENV == 'prod') {
-	return [
-		'class' => 'yii\db\Connection',
-		'dsn' => 'mysql:host=localhost;dbname=cd91333_kotofey',
-		'username' => 'cd91333_kotofey',
-		'password' => '123qweR%',
-		'charset' => 'utf8',
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=kotofey',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
 
-		// Schema cache options (for production environment)
-		'enableSchemaCache' => true,
-		'schemaCacheDuration' => 60,
-		'schemaCache' => 'cache',
-	];
-}
-if (YII_ENV == 'dev' and empty($_SERVER['DOCUMENT_ROOT']) or YII_ENV == 'dev') {
-	return [
-		'class' => 'yii\db\Connection',
-		'dsn' => 'mysql:host=localhost;dbname=kotofey_shop',
-		'username' => 'root',
-		'password' => '',
-		'charset' => 'utf8',
-
-		// Schema cache options (for production environment)
-		'enableSchemaCache' => true,
-		'schemaCacheDuration' => 60,
-		'schemaCache' => 'cache',
-	];
-}
+    // Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
+];
