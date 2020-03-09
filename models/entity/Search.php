05fe217e9bc3b8c6953fe20040e05df700de2d96
mijeko <123qweR%1>
2020-03-09 15:15:12 +0700

    update

--- models/entity/Search.php
+++ models/entity/Search.php
@@ -11,69 +11,69 @@ use yii\db\ActiveRecord;
 
 class Search extends Model
 {
-	public $search;
-	public $category;
-	public $pricefrom;
-	public $priceto;
-	public $save_history = false;
+    public $search;
+    public $category;
+    public $pricefrom;
+    public $priceto;
+    public $save_history = false;
 
-	public function rules()
-	{
-		return [
-			[['search', 'category', 'pricefrom', 'priceto'], 'string'],
+    public function rules()
+    {
+        return [
+            [['search', 'category', 'pricefrom', 'priceto'], 'string'],
 
-			[['save_history'], 'boolean'],
-		];
-	}
+            [['save_history'], 'boolean'],
+        ];
+    }
 
-	public function attributeLabels()
-	{
-		return [
-			'search' => 'Название товара',
-		];
-	}
+    public function attributeLabels()
+    {
+        return [
+            'search' => 'Название товара',
+        ];
+    }
 
-	public function search()
-	{
-		$products = Product::find();
-		$products = $this->setFilter($products);
+    public function search()
+    {
+        $products = Product::find()->where(['active' => 1]);
+        $products = $this->setFilter($products);
 
-		return $products->all();
-	}
+        return $products->all();
+    }
 
-	public function setFilter(ActiveQuery $products)
-	{
-		if (!empty($this->search)) {
+    public function setFilter(ActiveQuery $products)
+    {
+        if (!empty($this->search)) {
 
-			$phrase = $this->search;
+            $phrase = $this->search;
 
-			$products->where(['like', 'name', $phrase]);
-			$products->orWhere(['like', 'feed', $phrase]);
+            $products->where(['like', 'name', $phrase]);
+            $products->orWhere(['like', 'feed', $phrase]);
 
-			if ($products->count() == 0) {
-				$products->orWhere(['like', 'name', explode(" ", $phrase)]);
-				$products->orWhere(['like', 'feed', explode(" ", $phrase)]);
-			}
-		}
+            if ($products->count() == 0) {
+                $products->orWhere(['like', 'name', explode(" ", $phrase)]);
+                $products->orWhere(['like', 'feed', explode(" ", $phrase)]);
+            }
+        }
 
 
-		if ($this->save_history == true) {
+        if ($this->save_history == true) {
 
-			$SearchQuery = new SearchQuery();
-			$SearchQuery->text = $phrase;
-			$SearchQuery->count_find = $products->count();
-			$SearchQuery->ip = $_SERVER['REMOTE_ADDR'];
+            $SearchQuery = new SearchQuery();
+            $SearchQuery->text = $phrase;
+            $SearchQuery->count_find = $products->count();
+            $SearchQuery->ip = $_SERVER['REMOTE_ADDR'];
 
-			if (!\Yii::$app->user->isGuest) {
-				$SearchQuery->user_id = \Yii::$app->user->id;
-			}
+            if (!\Yii::$app->user->isGuest) {
+                $SearchQuery->user_id = \Yii::$app->user->id;
+            }
 
-			if ($SearchQuery->validate()) {
-				$SearchQuery->save();
-			}
-		}
+            if ($SearchQuery->validate()) {
+                $SearchQuery->save();
+            }
+        }
 
-		return $products;
+        return $products;
 
-	}
+    }
 }
