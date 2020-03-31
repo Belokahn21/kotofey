e12c7222230d0aef167040b1c81158d9835e960b
mijeko <123qweR%1>
2020-03-31 16:19:15 +0700

    update

--- views/admin/_forms/_promo.php
+++ views/admin/_forms/_promo.php
@@ -1,4 +1,4 @@
-<?= $form->field($model, 'is_active'); ?>
+<?= $form->field($model, 'is_active')->checkbox(); ?>
 <?= $form->field($model, 'code'); ?>
 <?= $form->field($model, 'discount'); ?>
 <?= $form->field($model, 'count'); ?>
