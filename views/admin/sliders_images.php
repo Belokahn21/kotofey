c0d6555689542c1d1a21fcfce9333a591c7eac86
mijeko <123qweR%1>
2020-03-31 14:48:40 +0700

    update

--- views/admin/sliders_images.php
+++ views/admin/sliders_images.php
@@ -14,55 +14,62 @@ $this->title = Title::showTitle("Изображения слайдеров"); ?>
     <section>
         <h1 class="title">Изображения слайдеров</h1>
         <div class="product-form">
-			<?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
-			<?= $this->render('_forms/_slider-images', [
-				'form' => $form,
-				'model' => $model
-			]) ?>
-			<?= Html::submitButton('Добавить', ['class' => 'btn-main']); ?>
-			<?php ActiveForm::end(); ?>
+            <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+            <?= $this->render('_forms/_slider-images', [
+                'form' => $form,
+                'model' => $model
+            ]) ?>
+            <?= Html::submitButton('Добавить', ['class' => 'btn-main']); ?>
+            <?php ActiveForm::end(); ?>
         </div>
     </section>
     <div class="clearfix"></div>
     <h2>Список изображений</h2>
 <?php echo \yii\grid\GridView::widget([
-	'dataProvider' => $dataProvider,
-	'filterModel' => $searchModel,
-	'emptyText' => 'Сладеры отсутствуют',
-	'columns' => [
-		'id',
-		'active',
-		'text',
-		'description',
-		'link',
-		[
-			'attribute' => 'slider_id',
-			'format' => 'raw',
-			'value' => function ($model) {
-				return Html::a(Sliders::findOne($model->slider_id)->name,
-					Url::to(['admin/sliders', 'id' => $model->slider_id]));
-			}
-		],
-		[
-			'attribute' => 'created_at',
-			'format' => ['date', 'dd.MM.YYYY'],
-			'options' => ['width' => '200']
-		],
-		[
-			'class' => 'yii\grid\ActionColumn',
-			'buttons' => [
-				'view' => function ($url, $model, $key) {
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'emptyText' => 'Сладеры отсутствуют',
+    'columns' => [
+        'id',
+        'active',
+        'text',
+        'description',
+        'link',
+        [
+            'attribute' => 'slider_id',
+            'format' => 'raw',
+            'value' => function ($model) {
+                return Html::a(Sliders::findOne($model->slider_id)->name,
+                    Url::to(['admin/sliders', 'id' => $model->slider_id]));
+            }
+        ],
+        [
+            'attribute' => 'image',
+            'format' => 'raw',
+            'value' => function ($model) {
+                return Html::img($model->image, ['width' => 200]);
+            }
+        ],
+        [
+            'attribute' => 'created_at',
+            'format' => ['date', 'dd.MM.YYYY'],
+            'options' => ['width' => '200']
+        ],
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'buttons' => [
+                'view' => function ($url, $model, $key) {
 //                    return Html::img('/images/eye.png', ['class' => 'grid-view-img feedback-view']);
-				},
-				'update' => function ($url, $model, $key) {
-					return Html::a('<i class="far fa-eye"></i>',
-						Url::to(['admin/sliderimages', 'id' => $model->id]));
-				},
-				'delete' => function ($url, $model, $key) {
-					return Html::a('<i class="fas fa-trash-alt"></i>',
-						Url::to(["/admin/sliderimages/", 'id' => $key, 'action' => 'delete']));
-				},
-			]
-		],
-	],
+                },
+                'update' => function ($url, $model, $key) {
+                    return Html::a('<i class="far fa-eye"></i>',
+                        Url::to(['admin/sliderimages', 'id' => $model->id]));
+                },
+                'delete' => function ($url, $model, $key) {
+                    return Html::a('<i class="fas fa-trash-alt"></i>',
+                        Url::to(["/admin/sliderimages/", 'id' => $key, 'action' => 'delete']));
+                },
+            ]
+        ],
+    ],
 ]); ?>
