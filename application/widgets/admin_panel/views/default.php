6badc488c425d2b193360c767b58dd79dc61c999
Konstantin <popugau@gmail.com>
2020-04-30 12:18:48 +0700

    update

--- application/widgets/admin_panel/views/default.php
+++ application/widgets/admin_panel/views/default.php
@@ -10,7 +10,7 @@ use yii\helpers\Url;
     <ul class="admin-panel-list">
         <li class="admin-panel-list__item link"><a href="/">Сайт</a></li>
         <li class="admin-panel-list__item link"><a href="/admin/">Панель управления</a></li>
-        <li class="admin-panel-list__item link"><a href="/admin/order/">Зказы <span class="actual-count"><?= Order::find()->count(); ?></span></a></li>
+        <li class="admin-panel-list__item link"><a href="<?= Url::to(['/admin/order/order-backend/index']); ?>">Заказы <span class="actual-count"><?= Order::find()->count(); ?></span></a></li>
         <li class="admin-panel-list__item link"><a href="/admin/catalog/">Товары <span class="actual-count"><?= Product::find()->count(); ?></span></a></li>
         <li class="admin-panel-list__item link"><a href="/admin/user/">Пользователи <span class="actual-count"><?= User::find()->count(); ?></span></a></li>
         <li class="admin-panel-list__item"><a href="#"><span>TS: <span class="admin-panel-list__item-ts"><?= time(); ?></span></span></a></li>
