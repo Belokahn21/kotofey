8ddf465b59ae1b1eed3824ffc2120c9748996b05
mijeko <123qweR%1>
2020-05-07 22:53:31 +0700

    update

---
+++ markup/src/backend/js/ecmascript6/core.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {liked: false};
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return e(
+            'button',
+            {onClick: () => this.setState({liked: true})},
+            'Like'
+        );
+    }
+}
+
+const domContainer = document.querySelector('.content');
+ReactDom.render(e(LikeButton), domContainer);
