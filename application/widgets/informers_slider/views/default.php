da2f41088d7423214e1eba5b61c31d55a247db46
Konstantin <popugau@gmail.com>
2020-05-12 18:45:19 +0700

    update

--- application/widgets/informers_slider/views/default.php
+++ application/widgets/informers_slider/views/default.php
@@ -2,31 +2,23 @@
 
 use app\models\entity\ProductPropertiesValues;
 
+/* @var $providers \yii\db\ActiveQuery */
+
 ?>
 <?php if ($this->beginCache('providers-cache', ['duration' => 3600 * 24 * 7])): ?>
-    <div class="container-fluid providers-wrap">
-        <h1 class="homepage-providers__title">В продаже корма для животных известных производителей зоотоваров</h1>
-        <div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
-            <a class="btn-nav" href="#multi-item-example" data-slide="prev"><i class="fa fa-chevron-left"></i></a>
-            <div class="carousel-inner" role="listbox">
-                <?php foreach ($providers->batch(6) as $iterI => $batchProviders): ?>
-                    <div class="carousel-item<?= ($iterI == 1 ? ' active' : ''); ?>">
-                        <div class="row homepage-providers">
-                            <?php foreach ($batchProviders as $iter => $provider): ?>
-                                <div class="col-2<?= ($iter > 1 ? ' clearfix d-none d-md-block' : ''); ?>">
-                                    <div class="card">
-                                        <a class="homepage-providers__link" href="<?= $provider->link; ?>">
-                                            <img class="homepage-providers__image" src="/upload/<?= $provider->image; ?>" title="<?= $provider->name; ?>" alt="<?= $provider->name; ?>">
-                                            <div class="homepage-providers__detail">К ассортимену (<?= ProductPropertiesValues::find()->where(['value' => $provider->id])->count('product_id'); ?>)</div>
-                                        </a>
-                                    </div>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-            <a class="btn-nav" href="#multi-item-example" data-slide="next"><i class="fa fa-chevron-right"></i></a>
+    <div class="companies-wrap">
+        <h2 class="homepage-providers__title">В продаже корма для животных известных производителей зоотоваров</h2>
+        <div class="swiper-container">
+            <ul class="companies swiper-wrapper">
+				<?php foreach ($providers->all() as $provider): ?>
+                    <li class="companies__item swiper-slide">
+                        <img class="companies__image" src="/upload/<?= $provider->image; ?>" alt="<?= $provider->name; ?>" title="<?= $provider->name; ?>">
+                        <a href="<?= $provider->link; ?>" class="companies__link">К ассортименту (<?= ProductPropertiesValues::find()->where(['value' => $provider->id])->count('product_id'); ?>)</a>
+                    </li>
+				<?php endforeach; ?>
+            </ul>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
         </div>
     </div>
-    <?php $this->endCache(); endif; ?>
+	<?php $this->endCache(); endif; ?>
