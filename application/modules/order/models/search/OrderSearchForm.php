9ceeff060a85b4ffe6c41c31e30713b740566655
mijeko <123qweR%1>
2020-05-29 15:40:44 +0700

    update

--- application/modules/order/models/search/OrderSearchForm.php
+++ application/modules/order/models/search/OrderSearchForm.php
@@ -11,43 +11,44 @@ use yii\data\ActiveDataProvider;
 class OrderSearchForm extends Order
 {
 
-	public static function tableName()
-	{
-		return "orders";
-	}
+    public static function tableName()
+    {
+        return "orders";
+    }
 
-	public function rules()
-	{
-		return [
-			[['delivery_id', 'payment_id'], 'string'],
-			[['user_id'], 'integer'],
-		];
-	}
+    public function rules()
+    {
+        return [
+            [['delivery_id', 'payment_id'], 'string'],
+            [['user_id', 'phone'], 'integer'],
+        ];
+    }
 
-	public function scenarios()
-	{
-		return Model::scenarios();
-	}
+    public function scenarios()
+    {
+        return Model::scenarios();
+    }
 
 
-	public function search($params)
-	{
-		$query = Order::find()->orderBy(['created_at' => SORT_DESC]);
+    public function search($params)
+    {
+        $query = Order::find()->orderBy(['created_at' => SORT_DESC]);
 
-		$dataProvider = new ActiveDataProvider([
-			'query' => $query,
-		]);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
 
-		if (!($this->load($params) && $this->validate())) {
-			return $dataProvider;
-		}
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
 
-		$query->andFilterWhere(['like', 'delivery_id', $this->delivery_id])
-			->andFilterWhere(['user_id' => $this->user_id])
-			->andFilterWhere(['like', 'payment_id', $this->payment_id]);
+        $query->andFilterWhere(['like', 'delivery_id', $this->delivery_id])
+            ->andFilterWhere(['user_id' => $this->user_id])
+            ->andFilterWhere(['phone' => $this->phone])
+            ->andFilterWhere(['like', 'payment_id', $this->payment_id]);
 
-		return $dataProvider;
-	}
+        return $dataProvider;
+    }
 
 
 }
