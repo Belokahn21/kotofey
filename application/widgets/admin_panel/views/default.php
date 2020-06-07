9fa8385f54bb8806686f5da00b4d67bccc65ff62
mijeko <123qweR%1>
2020-06-07 17:39:17 +0700

    update

--- application/widgets/admin_panel/views/default.php
+++ application/widgets/admin_panel/views/default.php
@@ -11,8 +11,8 @@ use yii\helpers\Url;
         <li class="admin-panel-list__item link"><a href="/">Сайт</a></li>
         <li class="admin-panel-list__item link"><a href="/admin/">Панель управления</a></li>
         <li class="admin-panel-list__item link"><a href="<?= Url::to(['/admin/order/order-backend/index']); ?>">Заказы <span class="actual-count"><?= Order::find()->count(); ?></span></a></li>
-        <li class="admin-panel-list__item link"><a href="/admin/catalog/">Товары <span class="actual-count"><?= Product::find()->count(); ?></span></a></li>
-        <li class="admin-panel-list__item link"><a href="/admin/user/">Пользователи <span class="actual-count"><?= User::find()->count(); ?></span></a></li>
+        <li class="admin-panel-list__item link"><a href="<?= Url::to(['/admin/catalog/product-backend/index']); ?>">Товары <span class="actual-count"><?= Product::find()->count(); ?></span></a></li>
+        <li class="admin-panel-list__item link"><a href="<?= Url::to(['/admin/user/user-backend/index']); ?>">Пользователи <span class="actual-count"><?= User::find()->count(); ?></span></a></li>
         <li class="admin-panel-list__item"><a href="#"><span>TS: <span class="admin-panel-list__item-ts"><?= time(); ?></span></span></a></li>
         <li class="admin-panel-list__item"><a href="#">Сегодня: <?= date('d.m.Y'); ?></a></li>
         <li class="admin-panel-list__item"><a href="<?= Url::to(['admin/cache']) ?>">Сбросить кеш</a></li>
