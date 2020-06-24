9ac1144987f1e3cdc44903fd193f8a21576827e2
Konstantin <popugau@gmail.com>
2020-06-24 14:55:46 +0700

    update

--- markup/src/backend/js/ecmascript6/core.js
+++ markup/src/backend/js/ecmascript6/core.js
@@ -1,1 +1,12 @@
 import search from '../reactjs/components/search';
+
+let buttonToggleSlider = document.querySelector('.js-toggle-sidebar');
+if (buttonToggleSlider) {
+	buttonToggleSlider.addEventListener('click', (event) => {
+		let sideElement = document.querySelector('.left-side');
+
+		if (sideElement) {
+			sideElement.classList.toggle('is-active');
+		}
+	});
+}
