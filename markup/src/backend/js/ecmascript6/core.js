725e3f55f7bede193ba92fde937285705912da72
Konstantin <popugau@gmail.com>
2020-06-30 18:50:33 +0700

    update

--- markup/src/backend/js/ecmascript6/core.js
+++ markup/src/backend/js/ecmascript6/core.js
@@ -3,7 +3,7 @@ import search from '../reactjs/components/sidebar';
 let buttonToggleSlider = document.querySelector('.js-toggle-sidebar');
 if (buttonToggleSlider) {
 	buttonToggleSlider.addEventListener('click', (event) => {
-		let sideElement = document.querySelector('.left-side');
+		let sideElement = document.querySelector('.left-sidebar-container');
 
 		if (sideElement) {
 			sideElement.classList.toggle('is-active');
--- markup/src/backend/js/reactjs/components/sidebar/index.js
+++ markup/src/backend/js/reactjs/components/sidebar/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Search from "../search";
 
 class Sidebar extends React.Component {
-    render() {
-        return (
-            <div>Разгоночный</div>
-        )
-    }
+	render() {
+		return (
+			<aside className="left-side">
+				<button className="button-toggle-sidebar js-toggle-sidebar" type="button">Меню</button>
+				<div className="left-sidebar-container">
+					<div className="sidebar-title">Поиск по сайту</div>
+					<Search/>
+					<div className="sidebar-title">Быстрый доступ</div>
+					<ul className="fast-links">
+						<li className="fast-links__item">
+							<div className="point"></div>
+							<a className="fast-links__link" href="javascript:void(0);"><i className="fas fa-receipt"></i></a></li>
+						<li className="fast-links__item"><a className="fast-links__link" href="javascript:void(0);"><i className="fas fa-cubes"></i></a></li>
+					</ul>
+					<div className="sidebar-title">Меню</div>
+					<ul className="menu">
+						<li className="menu__item"><a className="menu__link" href="javascript:void(0);"><span>Сайт</span><span className="menu__icon"><i className="fas fa-globe-americas"></i></span></a></li>
+						<li className="menu__item"><a className="menu__link" href="javascript:void(0);"><span>Рабочий стол</span><span className="menu__icon"><i className="fas fas fa-tachometer-alt"></i></span></a></li>
+						<li className="menu__item"><a className="menu__link" href="javascript:void(0);"><span>Магазин</span><span className="menu__icon"><i className="fas fa-store"></i></span>
+							<ul className="menu-sub">
+								<li className="menu-sub__item"><a className="menu-sub__link" href="javascript:void(0);"><span>Заказы</span><span className="menu__icon"><i className="fas fa-store"></i></span></a></li>
+								<li
+									className="menu-sub__item"><a className="menu-sub__link" href="javascript:void(0);"><span>Товары</span><span className="menu__icon"><i className="fas fa-store"></i></span></a></li>
+								<li className="menu-sub__item"><a className="menu-sub__link" href="javascript:void(0);"><span>Поставщики</span><span className="menu__icon"><i className="fas fa-store"></i></span></a></li>
+							</ul>
+						</a>
+						</li>
+					</ul>
+				</div>
+			</aside>
+		)
+	}
 }
+
 ReactDom.render(<Sidebar/>, document.querySelector('.left-side-react'));
