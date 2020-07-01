d34534319c4e1426217fcb20b5eb6386057cae4e
Konstantin <popugau@gmail.com>
2020-07-01 14:03:00 +0700

    update

--- application/modules/catalog/views/__item.php
+++ application/modules/catalog/views/__item.php
@@ -12,26 +12,26 @@ use app\modules\bonus\models\services\BonusByBuyService;
 ?>
 
 <li class="catalog-list__item">
-    <?php if ($product->discount_price): ?>
+	<?php if ($product->discount_price): ?>
         <div id="burst-12">
             <div class='dis'>
-                <?= floor((($product->discount_price - $product->price) / $product->price) * 100); ?>%
+				<?= floor((($product->discount_price - $product->price) / $product->price) * 100); ?>%
             </div>
         </div>
-    <?php endif; ?>
+	<?php endif; ?>
 
-    <?php if ($weight = ProductPropertiesValues::findOne(['product_id' => $product->id, 'property_id' => '2'])): ?>
+	<?php if ($weight = ProductPropertiesValues::findOne(['product_id' => $product->id, 'property_id' => '2'])): ?>
         <div class="catalog-list__weight">
-            <?= $weight->value; ?> КГ
+			<?= $weight->value; ?> КГ
         </div>
-    <?php endif; ?>
+	<?php endif; ?>
 
     <a href="<?= $product->detail; ?>" class="catalog-list__link">
-        <?php if (!empty($product->image) and is_file(Yii::getAlias('@webroot/upload/') . $product->image)): ?>
+		<?php if (!empty($product->image) and is_file(Yii::getAlias('@webroot/upload/') . $product->image)): ?>
             <img class="catalog-list__image" src="/upload/<?= $product->image; ?>" alt="<?= $product->name; ?>" title="<?= $product->name; ?>">
-        <?php else: ?>
+		<?php else: ?>
             <img class="catalog-list__image" src="/upload/images/not-image.png" alt="<?= $product->name; ?>" title="<?= $product->name; ?>">
-        <?php endif; ?>
+		<?php endif; ?>
     </a>
 
     <a href="<?= $product->detail; ?>" class="catalog-list__link">
@@ -40,15 +40,15 @@ use app\modules\bonus\models\services\BonusByBuyService;
     <div class="catalog-list__group-price">
         <div class="catalog-list__price">
             <div class="value"><?= Price::format(ProductHelper::getResultPrice($product)); ?> <?= Currency::getInstance()->show(); ?></div>
-            <?php if (BonusByBuyService::isActive()): ?>
+			<?php if (BonusByBuyService::isActive()): ?>
                 <div class="bonus"><?= DiscountHelper::calcBonus($product->price); ?> бонуса</div>
-            <?php endif; ?>
+			<?php endif; ?>
         </div>
 
         <div class="catalog-list__group-cart">
             <div onclick="ym(55089223, 'reachGoal', 'basket', {
-            	name: '<?=$product->name;?>'
-            }); return true;" class="product-button product-add-basket<?= (!Basket::getInstance()->exist($product->id)) ? '' : ' hide'; ?>" data-product="<?= $product->id; ?>">
+                    name: '<?= $product->name; ?>'
+                    }); return true;" class="product-button product-add-basket<?= (!Basket::getInstance()->exist($product->id)) ? '' : ' hide'; ?>" data-product="<?= $product->id; ?>">
                 В корзину
             </div>
 
@@ -71,7 +71,7 @@ use app\modules\bonus\models\services\BonusByBuyService;
             </div>
 
             <ul class="summary">
-                <li class="summary__item available">В наличии</li>
+                <li class="summary__item available">В наличии <?= ($product->count > 0 ? $product->count . " шт." : ""); ?></li>
                 <li class="summary__item">Арткиул: <?= $product->article; ?></li>
             </ul>
 
