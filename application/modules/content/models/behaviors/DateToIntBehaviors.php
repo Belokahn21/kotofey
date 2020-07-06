2a42113f27cda173a486356712d9874c5c78039c
Konstantin <popugau@gmail.com>
2020-07-06 15:03:30 +0700

    update

--- application/modules/content/models/behaviors/DateToIntBehaviors.php
+++ application/modules/content/models/behaviors/DateToIntBehaviors.php
@@ -18,13 +18,21 @@ class DateToIntBehaviors extends AttributeBehavior
 
 	public function setValue($event)
 	{
-		$this->owner->start_at = (strtotime($this->owner->start_at . " 00:00:00"));
-		$this->owner->end_at = (strtotime($this->owner->end_at . " 00:00:00"));
+		if (!empty($this->owner->start_at)) {
+			$this->owner->start_at = (strtotime($this->owner->start_at . " 00:00:00"));
+		}
+		if (!empty($this->owner->end_at)) {
+			$this->owner->end_at = (strtotime($this->owner->end_at . " 00:00:00"));
+		}
 	}
 
 	public function prepareValue($event)
 	{
-		$this->owner->start_at = date('d.m.Y', $this->owner->start_at);
-		$this->owner->end_at = date('d.m.Y', $this->owner->end_at);
+		if (!empty($this->owner->start_at)) {
+			$this->owner->start_at = date('d.m.Y', $this->owner->start_at);
+		}
+		if (!empty($this->owner->end_at)) {
+			$this->owner->end_at = date('d.m.Y', $this->owner->end_at);
+		}
 	}
 }
