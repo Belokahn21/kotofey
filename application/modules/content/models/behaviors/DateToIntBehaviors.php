81d21913ee390c424ed1cbfe38107aa7d01fe4d7
Konstantin <popugau@gmail.com>
2020-07-06 11:37:40 +0700

    update

--- application/modules/content/models/behaviors/DateToIntBehaviors.php
+++ application/modules/content/models/behaviors/DateToIntBehaviors.php
@@ -7,23 +7,24 @@ use yii\db\ActiveRecord;
 
 class DateToIntBehaviors extends AttributeBehavior
 {
-    public function events(){
-        return [
-            ActiveRecord::EVENT_BEFORE_VALIDATE => 'setValue',
-            ActiveRecord::EVENT_AFTER_FIND => 'prepareValue',
-        ];
-    }
+	public function events()
+	{
+		return [
+			ActiveRecord::EVENT_BEFORE_UPDATE => 'setValue',
+			ActiveRecord::EVENT_BEFORE_INSERT => 'setValue',
+			ActiveRecord::EVENT_AFTER_FIND => 'prepareValue',
+		];
+	}
 
-    public function setValue($event){
-//        $this->owner->start_at = rand();
-//        $this->owner->end_at = rand();
+	public function setValue($event)
+	{
+		$this->owner->start_at = (strtotime($this->owner->start_at . " 00:00:00"));
+		$this->owner->end_at = (strtotime($this->owner->end_at . " 00:00:00"));
+	}
 
-        $this->owner->start_at = intval(strtotime($this->owner->start_at." 00:00:00"));
-        $this->owner->end_at = intval(strtotime($this->owner->end_at." 00:00:00"));
-    }
-    public function prepareValue($event){
-
-        $this->owner->start_at = date('d.m.Y',$this->owner->start_at);
-        $this->owner->end_at = date('d.m.Y',$this->owner->end_at);
-    }
+	public function prepareValue($event)
+	{
+		$this->owner->start_at = date('d.m.Y', $this->owner->start_at);
+		$this->owner->end_at = date('d.m.Y', $this->owner->end_at);
+	}
 }
