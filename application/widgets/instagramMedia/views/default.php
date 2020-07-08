925ac193b65a70d79f2c702750ebf09d6c2e352d
Konstantin <popugau@gmail.com>
2020-07-08 10:50:11 +0700

    update

--- application/widgets/instagramMedia/views/default.php
+++ application/widgets/instagramMedia/views/default.php
@@ -1,4 +1,7 @@
 <?php
+
+use yii\helpers\StringHelper;
+
 /* @var $media StdClass */
 ?>
 <div class="instagram-media">
@@ -8,9 +11,11 @@
     <div class="swiper-container swiper-instagram-container">
         <div class="swiper-wrapper">
 			<?php foreach ($media->data as $mediaItem): ?>
-				<?php $title = @($mediaItem->caption ? \yii\helpers\StringHelper::truncate($mediaItem->caption, 40, '...') : "Без названия"); ?>
+				<?php $title = @($mediaItem->caption ? StringHelper::truncate($mediaItem->caption, 40, '...') : "Без названия"); ?>
                 <div class="swiper-slide swiper-instagram__slide">
-                    <img class="swiper-instagram__image" src="<?= $mediaItem->media_url; ?>">
+                    <a class="swiper-instagram__image-wrap" href="<?= $mediaItem->media_url; ?>" data-lightbox="roadtrip">
+                        <img class="swiper-instagram__image" src="<?= $mediaItem->media_url; ?>">
+                    </a>
                     <a title="<?= $title; ?>" class="swiper-instagram__link" href="<?= $mediaItem->permalink; ?>" target="_blank">
 						<?= $title; ?>
                     </a>
