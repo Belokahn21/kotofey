5cff569fc7463e81d19810d58d4d10e4ddf6eff9
Konstantin <popugau@gmail.com>
2020-07-08 17:46:00 +0700

    update

--- markup/src/backend/js/reactjs/config.js
+++ markup/src/backend/js/reactjs/config.js
@@ -1,13 +1,13 @@
-// if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-// 	module.exports = {
-// 		restMenuGet: 'http://local.kotofey.store/menu/rest-backend/get/',
-// 		restMenuFastGet: 'http://local.kotofey.store/menu_fast/rest-backend/get/',
-// 		restSearchGet: "http://local.kotofey.store/rest/product/get/"
-// 	};
-// } else {
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
 	module.exports = {
-		restMenuGet: '/menu/rest-backend/get/',
-		restMenuFastGet: '/menu_fast/rest-backend/get/',
-		restSearchGet: "/rest/product/get/"
+		restMenuGet: 'http://local.kotofey.store/menu/rest-backend/get/',
+		restMenuFastGet: 'http://local.kotofey.store/menu_fast/rest-backend/get/',
+		restSearchGet: "http://local.kotofey.store/rest/product/get/"
 	};
-// }
+} else {
+	module.exports = {
+		restMenuGet: 'https://kotofey.store/menu/rest-backend/get/',
+		restMenuFastGet: 'https://kotofey.store/menu_fast/rest-backend/get/',
+		restSearchGet: "https://kotofey.store/rest/product/get/"
+	};
+}
