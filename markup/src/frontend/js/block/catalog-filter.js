28e5892c0c2c9bfa47768f3e4300c1088b4d27ff
Konstantin <popugau@gmail.com>
2020-07-23 15:22:55 +0700

    update

--- markup/src/frontend/js/block/catalog-filter.js
+++ markup/src/frontend/js/block/catalog-filter.js
@@ -10,24 +10,6 @@ if (formElement) {
         inputs.forEach((element) => {
             element.addEventListener('change', () => {
                 console.log('element changed');
-
-                // var url = new URL(location.href),
-                //     params = {}
-                // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-                //
-                // fetch(url, {
-                //     method: 'GET',
-                // });
-
-                var url = new URL(location.href)
-
-                var params = {lat:35.696233, long:139.570431} // or:
-                var params = [['lat', '35.696233'], ['long', '139.570431']]
-
-                url.search = new URLSearchParams(params).toString();
-
-                fetch(url)
-
             });
         })
     }
