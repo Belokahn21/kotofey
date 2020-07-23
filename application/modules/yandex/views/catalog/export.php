deb3e56a86c36fdf97080557b4d9a72a46319c81
Konstantin <popugau@gmail.com>
2020-07-23 15:57:59 +0700

    update

--- application/modules/yandex/views/catalog/export.php
+++ application/modules/yandex/views/catalog/export.php
@@ -25,7 +25,7 @@ use app\modules\catalog\models\helpers\ProductHelper;
             <?php endforeach; ?>
         </categories>
         <delivery-options>
-            <option cost="0" days="1" order-before="16"/>
+            <option cost="0" days="0" />
         </delivery-options>
         <cpa>1</cpa>
         <offers>
