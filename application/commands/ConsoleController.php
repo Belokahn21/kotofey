ad284226331f05d1de1bddb25011951b6d5ba11c
Konstantin <popugau@gmail.com>
2020-07-29 17:43:58 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -2,12 +2,25 @@
 
 namespace app\commands;
 
+use app\modules\catalog\models\entity\Product;
 use yii\console\Controller;
 
 class ConsoleController extends Controller
 {
 	public function actionRun()
 	{
+		$products = Product::find()->where(['vendor_id' => 12])->all();
+		foreach ($products as $product) {
+			$product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
+			$old = $product->price;
+			$product->price += 10;
+			if ($product->validate()) {
+				if ($product->update()) {
+					echo $product->name . '-' . $old . '-' . $product->price;
+					echo PHP_EOL;
+				}
+			}
+		}
 	}
 
 	public function actionClearCache()
