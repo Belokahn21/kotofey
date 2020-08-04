b51e0caaba517abc659ad0d292c6de978ce746d1
Konstantin <popugau@gmail.com>
2020-08-04 17:43:45 +0700

    update

--- markup/src/backend/js/ecmascript6/core.js
+++ markup/src/backend/js/ecmascript6/core.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
+
+window.$ = window.jQuery = $;
+
 import '../reactjs/components/sidebar';
 import '../reactjs/components/statistic';
 import '../reactjs/components/todo';
@@ -5,11 +9,11 @@ import '../reactjs/components/todo';
 import 'popper.js';
 import 'bootstrap';
 
-import datepicker from 'js-datepicker';
-import Inputmask from "maskedinput";
-
 import './block/order-load-product';
+import './block/phone-mask';
 import './block/notify';
+import './include/other-jquery';
+import './include/maskedinput';
 
 let buttonToggleSlider = document.querySelector('.js-toggle-sidebar');
 if (buttonToggleSlider) {
@@ -20,23 +24,4 @@ if (buttonToggleSlider) {
 			sideElement.classList.toggle('is-active');
 		}
 	});
-}
-
-let jsDatePicker = document.querySelector('.js-datepicker');
-if (jsDatePicker) {
-	const picker = datepicker(jsDatePicker)
-}
-
-let elements = document.querySelectorAll('.js-phone-mask');
-
-if (elements) {
-	elements.forEach((element) => {
-		element.textContent = element.textContent.replace(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/, '$1 ($2) $3 $4-$5');
-	});
-}
-
-let russsianPhone = document.querySelector(".js-mask-ru");
-if (russsianPhone) {
-	let im = new Inputmask("+7 (999) 999 99-99", {placeholder: "+7 (___) ___ __ __"});
-	im.mask(russsianPhone);
 }
---
+++ markup/src/backend/js/ecmascript6/include/other-jquery.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+
+window.$ = window.jQuery = $;
+
+import 'air-datepicker';
+
+// var myDatepicker = $('.js-datepicker').datepicker({
+// 	range: false,
+// 	showEvent: 'click',
+// 	onSelect: function onSelect(formattedDate, date, inst) {
+// 		inst.hide();
+// 	}
+// });
