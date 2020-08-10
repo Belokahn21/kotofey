7ac43fb61acae701ad46fd4d712156d0c0688726
small_jedi <info@kotofey.store>
2020-08-11 02:28:43 +0700

    update

--- application/modules/catalog/views/product/view.php
+++ application/modules/catalog/views/product/view.php
@@ -14,9 +14,9 @@ use app\modules\basket\widgets\addBasket\AddBasketWidget;
 
 $this->params['breadcrumbs'][] = ['label' => "Каталог", 'url' => ['/catalog/']];
 if ($category) {
-	foreach ($category->undersections() as $parents) {
-		$this->params['breadcrumbs'][] = ['label' => $parents->name, 'url' => ['/catalog/' . $parents->slug . "/"]];
-	}
+    foreach ($category->undersections() as $parents) {
+        $this->params['breadcrumbs'][] = ['label' => $parents->name, 'url' => ['/catalog/' . $parents->slug . "/"]];
+    }
 }
 $this->params['breadcrumbs'][] = ['label' => $product->name, 'url' => [$product->detail]];
 
@@ -26,46 +26,48 @@ $this->title = Title::showTitle($product->name);
 <div class="product-detail">
     <div class="product-detail-left">
         <div class="product-detail-gallery">
-            <a class="product-detail-gallery__link" href="<?= ProductHelper::getImageUrl($product); ?>" data-lightbox="roadtrip">
-                <img class="product-detail-gallery__image" src="<?= ProductHelper::getImageUrl($product); ?>" title="<?= $product->name; ?>" alt="<?= $product->name; ?>">
+            <a class="product-detail-gallery__link" href="<?= ProductHelper::getImageUrl($product); ?>"
+               data-lightbox="roadtrip">
+                <img class="product-detail-gallery__image" src="<?= ProductHelper::getImageUrl($product); ?>"
+                     title="<?= $product->name; ?>" alt="<?= $product->name; ?>">
             </a>
             <div class="product-detail-gallery__group">
-				<?php if ($product->images): ?>
-					<?php foreach (Json::decode($product->images) as $image): ?>
+                <?php if ($product->images): ?>
+                    <?php foreach (Json::decode($product->images) as $image): ?>
                         <a class="product-detail-gallery__link active" href="<?= $image; ?>" data-lightbox="roadtrip">
                             <img class="product-detail-gallery__image" src="<?= $image; ?>">
                         </a>
-					<?php endforeach; ?>
-				<?php endif; ?>
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </div>
         </div>
     </div>
     <div class="product-detail-right">
-		<?= Breadcrumbs::widget([
-			'homeLink' => [
-				'label' => 'Главная ',
-				'url' => Yii::$app->homeUrl,
-				'title' => 'Первая страница сайта зоомагазина Котофей',
-			],
-			'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-		]); ?>
+        <?= Breadcrumbs::widget([
+            'homeLink' => [
+                'label' => 'Главная ',
+                'url' => Yii::$app->homeUrl,
+                'title' => 'Первая страница сайта зоомагазина Котофей',
+            ],
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]); ?>
         <h1 class="product-detail__title"><?= $product->name; ?></h1>
-		<?= AddBasketWidget::widget([
-			'product_id' => $product->id,
-			'price' => $product->price,
-		]); ?>
-		<?php if ($properties): ?>
+        <?= AddBasketWidget::widget([
+            'product_id' => $product->id,
+            'price' => $product->price,
+        ]); ?>
+        <?php if ($properties): ?>
             <ul class="product-properties">
-				<?php foreach ($properties as $property): ?>
+                <?php foreach ($properties as $property): ?>
                     <li class="product-properties__line">
                         <div class="product-properties__key"><?= $property->property->name; ?></div>
                         <div class="product-properties__value"><?= $property->finalValue; ?></div>
                     </li>
-				<?php endforeach; ?>
+                <?php endforeach; ?>
             </ul>
-		<?php endif; ?>
+        <?php endif; ?>
 
-		<?php /*
+        <?php /*
         <ul class="collapse product-properties" id="collapseExample">
             <li class="product-properties__line">
                 <div class="product-properties__key">Артикул товара</div>
@@ -82,18 +84,25 @@ $this->title = Title::showTitle($product->name);
 </div>
 <nav class="product-tabs in-product">
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <a class="nav-item nav-link active" id="nav-description-tab" data-toggle="tab" href="#nav-description" role="tab" aria-controls="nav-description" aria-selected="true">Описание</a>
-        <a class="nav-item nav-link" id="nav-characteristics-tab" data-toggle="tab" href="#nav-characteristics" role="tab" aria-controls="nav-characteristics" aria-selected="false">Характеристики</a>
-        <a class="nav-item nav-link" id="nav-recommendations-tab" data-toggle="tab" href="#nav-recommendations" role="tab" aria-controls="nav-recommendations" aria-selected="false">Рекомендации</a>
-        <a class="nav-item nav-link" id="nav-delivery-tab" data-toggle="tab" href="#nav-delivery" role="tab" aria-controls="nav-delivery" aria-selected="false">Доставка</a>
-        <a class="nav-item nav-link" id="nav-payment-tab" data-toggle="tab" href="#nav-payment" role="tab" aria-controls="nav-payment" aria-selected="false">Оплата</a>
-        <a class="nav-item nav-link" id="nav-buy-tab" data-toggle="tab" href="#nav-buy" role="tab" aria-controls="nav-buy" aria-selected="false">Как купить?</a>
-        <a class="nav-item nav-link" id="nav-available-tab" data-toggle="tab" href="#nav-available" role="tab" aria-controls="nav-available" aria-selected="false">Наличие в магазинах</a>
+        <a class="nav-item nav-link active" id="nav-description-tab" data-toggle="tab" href="#nav-description"
+           role="tab" aria-controls="nav-description" aria-selected="true">Описание</a>
+        <a class="nav-item nav-link" id="nav-characteristics-tab" data-toggle="tab" href="#nav-characteristics"
+           role="tab" aria-controls="nav-characteristics" aria-selected="false">Характеристики</a>
+        <a class="nav-item nav-link" id="nav-recommendations-tab" data-toggle="tab" href="#nav-recommendations"
+           role="tab" aria-controls="nav-recommendations" aria-selected="false">Рекомендации</a>
+        <a class="nav-item nav-link" id="nav-delivery-tab" data-toggle="tab" href="#nav-delivery" role="tab"
+           aria-controls="nav-delivery" aria-selected="false">Доставка</a>
+        <a class="nav-item nav-link" id="nav-payment-tab" data-toggle="tab" href="#nav-payment" role="tab"
+           aria-controls="nav-payment" aria-selected="false">Оплата</a>
+        <a class="nav-item nav-link" id="nav-buy-tab" data-toggle="tab" href="#nav-buy" role="tab"
+           aria-controls="nav-buy" aria-selected="false">Как купить?</a>
+        <a class="nav-item nav-link" id="nav-available-tab" data-toggle="tab" href="#nav-available" role="tab"
+           aria-controls="nav-available" aria-selected="false">Наличие в магазинах</a>
     </div>
 </nav>
 <div class="tab-content product-tab-content" id="nav-tabContent">
     <div class="tab-pane fade show active" id="nav-description" role="tabpanel" aria-labelledby="nav-description-tab">
-		<?= $product->description; ?>
+        <?= $product->description; ?>
     </div>
     <div class="tab-pane fade" id="nav-characteristics" role="tabpanel" aria-labelledby="nav-characteristics-tab">
         Отсутсвует
@@ -111,7 +120,13 @@ $this->title = Title::showTitle($product->name);
         Отсутсвует
     </div>
     <div class="tab-pane fade" id="nav-available" role="tabpanel" aria-labelledby="nav-available-tab">
-        Отсутсвует
+        <ul class="in-stock-detail-product">
+            <?php foreach (\app\modules\stock\models\entity\Stocks::find()->where(['active' => 1])->all() as $item) : ?>
+                <li class="in-stock-detail-product__item"><?= $item->name; ?>(<?= $item->address; ?>) - <span class="green">В НАЛИЧИИ</span></li>
+            <?php endforeach; ?>
+
+
+        </ul>
     </div>
 </div>
 <?php /*
