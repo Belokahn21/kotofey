554016190e993be6d66062c693c4c63e9ef171e6
Konstantin <popugau@gmail.com>
2020-08-12 17:58:59 +0700

    update

--- application/modules/catalog/views/__item.php
+++ application/modules/catalog/views/__item.php
@@ -5,6 +5,7 @@ use app\models\tool\Currency;
 use app\modules\catalog\models\helpers\ProductHelper;
 use app\modules\basket\widgets\addBasket\AddBasketWidget;
 use app\modules\catalog\widgets\preview_properties\PreviewPropertiesWidget;
+use app\modules\favorite\models\entity\Favorite;
 
 /* @var $product \app\modules\catalog\models\entity\Product */
 
@@ -14,34 +15,45 @@ $isDiscount = $product->discount_price > 0;
 
 <li class="catalog__item">
     <div class="catalog-actions">
-        <div class="catalog-favorite js-toggle-class js-add-favorite" data-class-target="fas fa-heart" data-product-id="<?= $product->id; ?>"><i class="far fa-heart"></i></div>
-        <div class="catalog-compaare js-toggle-class js-add-compare" data-class-target="fas fa-balance-scale-right"><i class="fas fa-balance-scale-right"></i></div>
+		<?php if (Favorite::getInstance()->exist($product->id)): ?>
+            <div class="catalog-favorite js-toggle-class js-add-favorite" data-class-target="far fa-heart" data-product-id="<?= $product->id; ?>">
+                <i class="fas fa-heart"></i>
+            </div>
+		<?php else: ?>
+            <div class="catalog-favorite js-toggle-class js-add-favorite" data-class-target="fas fa-heart" data-product-id="<?= $product->id; ?>">
+                <i class="far fa-heart"></i>
+            </div>
+		<?php endif; ?>
+
+        <div class="catalog-compaare js-toggle-class js-add-compare" data-class-target="fas fa-balance-scale-right">
+            <i class="fas fa-balance-scale"></i>
+        </div>
     </div>
-    <?php if ($isDiscount): ?>
+	<?php if ($isDiscount): ?>
         <div class="catalog__discount"><span>-15%</span></div>
-    <?php endif; ?>
+	<?php endif; ?>
     <img class="catalog__image" src="<?= ProductHelper::getImageUrl($product); ?>">
     <div class="catalog__title">
         <a class="catalog__link" href="<?= $product->detail; ?>"><?= $product->name; ?></a>
     </div>
-    <?= PreviewPropertiesWidget::widget([
-        'product' => $product
-    ]); ?>
+	<?= PreviewPropertiesWidget::widget([
+		'product' => $product
+	]); ?>
     <div class="catalog__price-group">
-        <?php if ($isDiscount): ?>
+		<?php if ($isDiscount): ?>
             <div class="catalog__old-price"><?= Price::format($product->price); ?></div>
             <div class="catalog__new-price"><?= Price::format($product->discount_price); ?></div>
-        <?php else: ?>
+		<?php else: ?>
             <div class="catalog__price"><?= Price::format($product->price); ?></div>
-        <?php endif; ?>
+		<?php endif; ?>
         <div class="catalog__rate"><?= Currency::getInstance()->show(); ?> / шт</div>
     </div>
-    <?= AddBasketWidget::widget([
-        'product_id' => $product->id,
-        'price' => $product->price,
-        'showInfo' => false,
-        'showOneClick' => false,
-        'showControl' => false,
-        'showButton' => true,
-    ]) ?>
+	<?= AddBasketWidget::widget([
+		'product_id' => $product->id,
+		'price' => $product->price,
+		'showInfo' => false,
+		'showOneClick' => false,
+		'showControl' => false,
+		'showButton' => true,
+	]) ?>
 </li>
