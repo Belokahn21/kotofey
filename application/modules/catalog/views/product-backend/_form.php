795e076687a522b72ecdcab78f56f60ceb162dcc
small_jedi <info@kotofey.store>
2020-09-12 14:03:20 +0700

    update

--- application/modules/catalog/views/product-backend/_form.php
+++ application/modules/catalog/views/product-backend/_form.php
@@ -9,7 +9,7 @@ use app\modules\catalog\models\entity\ProductPropertiesValues;
 use yii\helpers\Json;
 use app\modules\vendors\models\entity\Vendor;
 use app\modules\settings\models\helpers\MarkupHelpers;
-use app\modules\catalog\widgets\FillFromVendor\FillFromVendorWidget;
+use app\modules\catalog\models\helpers\ProductHelper;
 
 /* @var $model \app\modules\catalog\models\entity\Product */
 /* @var $modelDelivery \app\modules\catalog\models\entity\ProductOrder */
@@ -31,40 +31,40 @@ use app\modules\catalog\widgets\FillFromVendor\FillFromVendorWidget;
         <div class="row">
             <div class="col-sm-6">
                 <div class="form-element">
-					<?= $form->field($model, 'name')->textInput(['placeholder' => 'Название'])->label(false); ?>
+                    <?= $form->field($model, 'name')->textInput(['placeholder' => 'Название'])->label(false); ?>
                 </div>
                 <div class="form-element">
-					<?= $form->field($model, 'description')->textarea(['placeholder' => 'Описание'])->label(false); ?>
-                </div>
-
-                <div class="form-element">
-					<?= $form->field($model, 'category_id')->dropDownList(ArrayHelper::map((new Category())->categoryTree(), 'id', 'name'), ['prompt' => 'Раздел товара'])->label(false); ?>
+                    <?= $form->field($model, 'description')->textarea(['placeholder' => 'Описание'])->label(false); ?>
                 </div>
 
                 <div class="form-element">
-					<?= $form->field($model, 'vendor_id')->dropDownList(ArrayHelper::map(Vendor::find()->all(), 'id', 'name'), ['prompt' => 'Поставщик'])->label(false); ?>
+                    <?= $form->field($model, 'category_id')->dropDownList(ArrayHelper::map((new Category())->categoryTree(), 'id', 'name'), ['prompt' => 'Раздел товара'])->label(false); ?>
+                </div>
+
+                <div class="form-element">
+                    <?= $form->field($model, 'vendor_id')->dropDownList(ArrayHelper::map(Vendor::find()->all(), 'id', 'name'), ['prompt' => 'Поставщик'])->label(false); ?>
                 </div>
 
                 <div class="row">
                     <div class="col-sm-2">
                         <div class="form-element">
-							<?= $form->field($model, 'discount_price')->textInput(['placeholder' => 'Цена со скидкой'])->label(false); ?>
+                            <?= $form->field($model, 'discount_price')->textInput(['placeholder' => 'Цена со скидкой'])->label(false); ?>
                         </div>
                     </div>
                     <div class="col-sm-2">
                         <div class="form-element">
-							<?= $form->field($model, 'base_price')->textInput(['placeholder' => 'Базовая цена'])->label(false); ?>
+                            <?= $form->field($model, 'base_price')->textInput(['placeholder' => 'Базовая цена'])->label(false); ?>
                         </div>
                     </div>
                     <div class="col-sm-2">
                         <div class="form-element">
-							<?= $form->field($model, 'purchase')->textInput(['id' => 'id-purchase', 'placeholder' => 'Закупочная цена'])->label(false); ?>
+                            <?= $form->field($model, 'purchase')->textInput(['id' => 'id-purchase', 'placeholder' => 'Закупочная цена'])->label(false); ?>
                         </div>
                     </div>
 
                     <div class="col-sm-3">
                         <div class="form-element">
-							<?= $form->field($model, 'price')->textInput(['id' => 'id-price', 'placeholder' => 'Цена продажи'])->label(false); ?>
+                            <?= $form->field($model, 'price')->textInput(['id' => 'id-price', 'placeholder' => 'Цена продажи'])->label(false); ?>
                         </div>
 
                         <div class="set-price">
@@ -75,13 +75,13 @@ use app\modules\catalog\widgets\FillFromVendor\FillFromVendorWidget;
 
                     <div class="col-sm-3">
                         <div class="form-element">
-							<?= $form->field($model, 'count')->textInput(['placeholder' => 'Количество'])->label(false); ?>
+                            <?= $form->field($model, 'count')->textInput(['placeholder' => 'Количество'])->label(false); ?>
                         </div>
                     </div>
                 </div>
 
                 <div class="form-element">
-					<?= $form->field($model, 'has_store')->checkbox()->label(false); ?>
+                    <?= $form->field($model, 'has_store')->checkbox()->label(false); ?>
                 </div>
             </div>
             <div class="col-sm-6">
@@ -89,16 +89,16 @@ use app\modules\catalog\widgets\FillFromVendor\FillFromVendorWidget;
                     Условия доставки заказа
                 </div>
                 <div class="form-element">
-					<?= $form->field($model, 'is_product_order')->checkbox()->label(false); ?>
+                    <?= $form->field($model, 'is_product_order')->checkbox()->label(false); ?>
                 </div>
 
                 <div class="form-element">
                     <div class="row">
                         <div class="col-sm-6">
-							<?= $form->field($modelDelivery, 'start')->dropDownList(ProductOrder::availableDays(), ['prompt' => 'Выбрать минимальное значение']); ?>
+                            <?= $form->field($modelDelivery, 'start')->dropDownList(ProductOrder::availableDays(), ['prompt' => 'Выбрать минимальное значение']); ?>
                         </div>
                         <div class="col-sm-6">
-							<?= $form->field($modelDelivery, 'end')->dropDownList(ProductOrder::availableDays(), ['prompt' => 'Выбрать максимальное значение']); ?>
+                            <?= $form->field($modelDelivery, 'end')->dropDownList(ProductOrder::availableDays(), ['prompt' => 'Выбрать максимальное значение']); ?>
                         </div>
                     </div>
                 </div>
@@ -111,90 +111,92 @@ use app\modules\catalog\widgets\FillFromVendor\FillFromVendorWidget;
     </div>
     <div class="tab-pane fade" id="nav-seo" role="tabpanel" aria-labelledby="nav-seo-tab">
         <div class="form-element">
-			<?= $form->field($model, 'seo_keywords'); ?>
+            <?= $form->field($model, 'seo_keywords'); ?>
         </div>
         <div class="form-element">
-			<?= $form->field($model, 'seo_description')->textarea(); ?>
+            <?= $form->field($model, 'seo_description')->textarea(); ?>
         </div>
         <div class="form-element">
-			<?= $form->field($model, 'feed')->textarea(['id' => 'feed-replace']); ?>
+            <?= $form->field($model, 'feed')->textarea(['id' => 'feed-replace']); ?>
         </div>
     </div>
     <div class="tab-pane fade" id="nav-gallery" role="tabpanel" aria-labelledby="nav-gallery-tab">
         <div class="row">
             <div class="col-sm-6">
                 <div class="form-image single">
-					<?php if ($model->image): ?>
-                        <img src="/upload/<?= $model->image; ?>" width="150">
-					<?php endif; ?>
+                    <?php if ($model->image): ?>
+                        <a target="_blank" href="<?= ProductHelper::getImageUrl($model) ?>">
+                            <img src="<?= ProductHelper::getImageUrl($model) ?>" width="150" title="<?= $model->name; ?>" alt="<?= $model->name; ?>">
+                        </a>
+                    <?php endif; ?>
                 </div>
-				<?= $form->field($model, 'image')->fileInput(); ?>
+                <?= $form->field($model, 'image')->fileInput(); ?>
             </div>
             <div class="col-sm-6">
                 <div class="form-image more">
-					<?php if ($model->images): ?>
-						<?php foreach (Json::decode($model->images) as $image): ?>
+                    <?php if ($model->images): ?>
+                        <?php foreach (Json::decode($model->images) as $image): ?>
                             <img src="<?= $image; ?>" width="150">
-						<?php endforeach; ?>
-					<?php endif; ?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
                 </div>
-				<?= $form->field($model, 'imagesFiles[]')->fileInput([
-					'multiple' => true,
-					'accept' => 'image/*'
-				]); ?>
+                <?= $form->field($model, 'imagesFiles[]')->fileInput([
+                    'multiple' => true,
+                    'accept' => 'image/*'
+                ]); ?>
             </div>
         </div>
     </div>
     <div class="tab-pane fade" id="nav-additional" role="tabpanel" aria-labelledby="nav-additional-tab">
         <div class="form-element">
-			<?= $form->field($model, 'code')->textInput(); ?>
+            <?= $form->field($model, 'code')->textInput(); ?>
         </div>
         <div class="form-element">
-			<?= $form->field($model, 'vitrine')->radioList(["Нет", "Да"]); ?>
+            <?= $form->field($model, 'vitrine')->radioList(["Нет", "Да"]); ?>
         </div>
         <div class="form-element">
-			<?= $form->field($model, 'active')->radioList(["Не активен", "Активен"]); ?>
+            <?= $form->field($model, 'active')->radioList(["Не активен", "Активен"]); ?>
         </div>
         <div class="form-element">
-			<?= $form->field($model, 'stock_id')->dropDownList(ArrayHelper::map(Stocks::find()->all(), 'id', 'name')) ?>
+            <?= $form->field($model, 'stock_id')->dropDownList(ArrayHelper::map(Stocks::find()->all(), 'id', 'name')) ?>
         </div>
         <div class="form-element">
-			<?= $form->field($model, 'threeDCode')->textarea(['rows' => 5]) ?>
+            <?= $form->field($model, 'threeDCode')->textarea(['rows' => 5]) ?>
         </div>
     </div>
     <div class="tab-pane fade" id="nav-props" role="tabpanel" aria-labelledby="nav-props-tab">
         <ul style="list-style: none; margin: 0; padding: 0;">
-			<?php try { ?>
+            <?php try { ?>
                 <ul style="list-style: none; margin: 0; padding: 0;">
-					<?php foreach ($properties as $property): ?>
-						<?php if ($property->type == 1): ?>
-							<?php $value = ProductPropertiesValues::findAll([
-								'product_id' => $model->id,
-								'property_id' => $property->id
-							]);
-							if ($value):
-								$model->properties[$property->id] = ArrayHelper::getColumn($value, 'value');
-							endif; ?>
-							<?php
-							$drop_down_params = ['prompt' => $property->name, 'multiple' => (boolean)$property->multiple];
-							if ((boolean)$property->multiple == true) {
-								$drop_down_params['size'] = 10;
-							}
-							?>
-							<?= $form->field($model, 'properties[' . $property->id . ']')->dropDownList(ArrayHelper::map(InformersValues::find()->where(['informer_id' => $property->informer_id])->orderBy(['created_at' => SORT_DESC])->all(), 'id', 'name'), $drop_down_params)->label($property->name); ?>
-						<?php else: ?>
-							<?php if ($value = ProductPropertiesValues::findOne(['product_id' => $model->id, 'property_id' => $property->id])): ?>
-								<?= $form->field($model, 'properties[' . $property->id . ']')->textInput(['value' => $value->value])->label($property->name); ?>
-							<?php else: ?>
-								<?= $form->field($model, 'properties[' . $property->id . ']')->textInput()->label($property->name); ?>
-							<?php endif; ?>
-						<?php endif; ?>
-					<?php endforeach; ?>
+                    <?php foreach ($properties as $property): ?>
+                        <?php if ($property->type == 1): ?>
+                            <?php $value = ProductPropertiesValues::findAll([
+                                'product_id' => $model->id,
+                                'property_id' => $property->id
+                            ]);
+                            if ($value):
+                                $model->properties[$property->id] = ArrayHelper::getColumn($value, 'value');
+                            endif; ?>
+                            <?php
+                            $drop_down_params = ['prompt' => $property->name, 'multiple' => (boolean)$property->multiple];
+                            if ((boolean)$property->multiple == true) {
+                                $drop_down_params['size'] = 10;
+                            }
+                            ?>
+                            <?= $form->field($model, 'properties[' . $property->id . ']')->dropDownList(ArrayHelper::map(InformersValues::find()->where(['informer_id' => $property->informer_id])->orderBy(['created_at' => SORT_DESC])->all(), 'id', 'name'), $drop_down_params)->label($property->name); ?>
+                        <?php else: ?>
+                            <?php if ($value = ProductPropertiesValues::findOne(['product_id' => $model->id, 'property_id' => $property->id])): ?>
+                                <?= $form->field($model, 'properties[' . $property->id . ']')->textInput(['value' => $value->value])->label($property->name); ?>
+                            <?php else: ?>
+                                <?= $form->field($model, 'properties[' . $property->id . ']')->textInput()->label($property->name); ?>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
                 </ul>
-			<?php } catch (ErrorException $exception) { ?>
-				<?= $exception->getMessage(); ?>
-				<?= $exception->getLine(); ?>
-			<?php } ?>
+            <?php } catch (ErrorException $exception) { ?>
+                <?= $exception->getMessage(); ?>
+                <?= $exception->getLine(); ?>
+            <?php } ?>
         </ul>
     </div>
 </div>
