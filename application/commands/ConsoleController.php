895a5485ae25cb43a6d1de4a5cf20d8d06354d33
small_jedi <info@kotofey.store>
2020-09-14 22:15:39 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -8,29 +8,29 @@ use yii\console\Controller;
 
 class ConsoleController extends Controller
 {
-	public function actionRun()
-	{
-		$products = Product::find()->all();
-		/* @var $product Product */
-		foreach ($products as $product) {
-			$product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
-			$product->discount_price = null;
+    public function actionRun()
+    {
+        $products = Product::find()->where(['name' => 'Мurkel'])->all();
+        /* @var $product Product */
+        foreach ($products as $product) {
+            $product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
+            $product->name = 'Murkel';
 
-			if ($product->validate()) {
-				$product->update();
-			} else {
-				Debug::p($product->name);
-				echo PHP_EOL;
-				Debug::p($product->code);
-				echo PHP_EOL;
-				Debug::p($product->getErrors());
-				return;
-			}
-		}
-	}
+            if ($product->validate()) {
+                $product->update();
+            } else {
+                Debug::p($product->name);
+                echo PHP_EOL;
+                Debug::p($product->code);
+                echo PHP_EOL;
+                Debug::p($product->getErrors());
+                return;
+            }
+        }
+    }
 
-	public function actionClearCache()
-	{
-		\Yii::$app->cache->flush();
-	}
+    public function actionClearCache()
+    {
+        \Yii::$app->cache->flush();
+    }
 }
