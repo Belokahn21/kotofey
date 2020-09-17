0d937b5792de5437c797e61b8b087df8018a688b
small_jedi <popugau@gmail.com>
2020-09-17 10:36:44 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -11,17 +11,17 @@ class ConsoleController extends Controller
 {
     public function actionRun()
     {
-        $products = Product::find()->where(['vendor_id' => 3])->orWhere(['like', 'name', 'royal canin'])->andWhere(['>', 'discount_price', 0])->select(['id', 'name'])->all();
+        $products = Product::find()->where(['vendor_id' => 1])->orWhere(['like', 'name', 'purina'])->andWhere(['>', 'discount_price', 0])->select(['id', 'name'])->all();
 
         foreach ($products as $product) {
             $row = new ProductPropertiesValues();
             $row->product_id = $product->id;
             $row->property_id = 11;
-            $row->value = '211';
+            $row->value = '210';
 
             if ($row->validate()) {
                 if ($row->save()) {
-                    Debug::p("Успешно вставлено");
+                    Debug::p("Успешно вставлено " . $product->name . PHP_EOL);
                 }
             } else {
                 Debug::p($row->getErrors());
