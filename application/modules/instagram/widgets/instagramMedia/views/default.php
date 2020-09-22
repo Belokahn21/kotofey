daa38664918ac71ae3ce5b642234ca7dc3695695
small_jedi <popugau@gmail.com>
2020-09-22 19:18:37 +0700

    update

--- application/modules/instagram/widgets/instagramMedia/views/default.php
+++ application/modules/instagram/widgets/instagramMedia/views/default.php
@@ -5,20 +5,20 @@ use yii\helpers\StringHelper;
 /* @var $media StdClass */
 
 ?>
-<?php if ($media->data): ?>
+<?php if (property_exists($media, 'data')): ?>
     <div class="page-title__group is-column">
         <h2 class="page-title">Наш Instagram</h2>
-		<?php /* <a class="page-title__link" href="javascript:void(0);">Все бренды</a> */ ?>
+        <?php /* <a class="page-title__link" href="javascript:void(0);">Все бренды</a> */ ?>
     </div>
     <div class="swiper-container instagram-container">
         <div class="swiper-wrapper instagram-wrapper">
-			<?php foreach ($media->data as $item): ?>
+            <?php foreach ($media->data as $item): ?>
                 <div class="instagram__slide swiper-slide">
                     <img class="instagram__image swiper-lazy" data-src="<?= @$item->media_url; ?>" alt="<?= @$item->caption; ?>" title="<?= @$item->caption; ?>"/>
                     <div class="swiper-lazy-preloader"></div>
                     <a class="instagram__title" href="<?= @$item->permalink; ?>" target="_blank"><?= @StringHelper::truncate($item->caption, 70, '...'); ?></a>
                 </div>
-			<?php endforeach; ?>
+            <?php endforeach; ?>
         </div>
     </div>
 <?php endif; ?>
