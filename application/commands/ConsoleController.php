57a7ed305c7619e475ddef5ba5f392b72041298e
small_jedi <popugau@gmail.com>
2020-09-30 16:21:07 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -4,29 +4,26 @@ namespace app\commands;
 
 use app\models\tool\Debug;
 use app\modules\catalog\models\entity\Product;
-use app\modules\catalog\models\entity\ProductPropertiesValues;
-use app\modules\catalog\models\entity\ProductSync;
 use yii\console\Controller;
 
 class ConsoleController extends Controller
 {
     public function actionRun()
     {
-//        ProductSync::deleteAll();
-//        $products = Product::find()->all();
-//
-//        foreach ($products as $product) {
-//            $product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
-//            $product->article = rand(100000, 999999);
-//
-//            if ($product->validate()) {
-//                if ($product->update()) {
-//                    echo "good" . PHP_EOL;
-//                }
-//            } else {
-//                print_r($product->getErrors());
-//            }
-//        }
+        $products = Product::find()->where(['>', 'discount_price', 0])->all();
+
+        foreach ($products as $product) {
+            $product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
+            $product->discount_price = 0;
+
+            if ($product->validate()) {
+                if ($product->update()) {
+                    echo $product->name . " - good" . PHP_EOL;
+                }
+            } else {
+                print_r($product->getErrors());
+            }
+        }
     }
 
     public function actionClearCache()
