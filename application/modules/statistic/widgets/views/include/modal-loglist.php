868acea22ec218b94e8a65f55a0b59cadb1c4f5a
small_jedi <popugau@gmail.com>
2020-09-30 16:44:11 +0700

    update

--- application/modules/statistic/widgets/views/include/modal-loglist.php
+++ application/modules/statistic/widgets/views/include/modal-loglist.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="log-listLabel">Что ищут люди?</h5>
+                <h5 class="modal-title" id="log-listLabel">Лог-лист сайта</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
