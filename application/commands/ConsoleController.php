e38f314761d6fd2bdaa41aebcaea7cb5ce29e89b
small_jedi <popugau@gmail.com>
2020-10-06 15:43:25 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -14,21 +14,20 @@ class ConsoleController extends Controller
     public function actionRun()
     {
         $products = Product::find();
-        $products->where(['vendor_id'=> Vendor::VENDOR_ID_PURINA]);
-//        $products->where(['<>', 'vendor_id', Vendor::VENDOR_ID_PURINA]);
-//        $products->where(['like', 'name', 'purina']);
-//        $products->orWhere(['like', 'name', 'cat chow']);
-//        $products->orWhere(['like', 'name', 'dog chow']);
+        $products->where(['vendor_id' => Vendor::VENDOR_ID_PURINA]);
         echo "Найдено: " . $products->count();
         $products = $products->all();
+
         foreach ($products as $product) {
+
+            $product->status_id = Product::STATUS_DRAFT;
 
 
             $product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
             if ($product->validate()) {
-//                if ($product->update()) {
-//                echo $product->name . " - good" . PHP_EOL;
-//                }
+                if ($product->update()) {
+                    echo $product->name . " - good" . PHP_EOL;
+                }
             } else {
                 print_r($product->getErrors());
             }
