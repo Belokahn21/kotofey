eebf57376c6dedfcd13b6e81c12b07e2e4165161
small_jedi <popugau@gmail.com>
2020-10-08 08:54:04 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -14,15 +14,14 @@ class ConsoleController extends Controller
     public function actionRun()
     {
         $products = Product::find();
-//        $products->where(['vendor_id' => Vendor::VENDOR_ID_PURINA]);
-        $products->where(['like', 'name', 'сибирская кошка']);
-        $products->orWhere(['like', 'name', 'brit premium']);
-        $products->orWhere(['like', 'name', 'brit premium']);
+        $products->where(['like', 'name', 'little one']);
+        $products->orWhere(['like', 'name', 'лежанка']);
+        $products->orWhere(['like', 'name', 'acana']);
+        $products->orWhere(['like', 'name', 'Brit Premium']);
         echo "Найдено: " . $products->count();
         $products = $products->all();
 
         foreach ($products as $product) {
-
             $product->vendor_id = Vendor::VENDOR_ID_SIBAGRO;
 
 
