e563c01947b186488bb110698dfeac8dfce994e4
small_jedi <popugau@gmail.com>
2020-10-09 10:53:40 +0700

    update

--- application/modules/site/components/behaviors/ImageUploadMinify.php
+++ application/modules/site/components/behaviors/ImageUploadMinify.php
@@ -4,6 +4,7 @@ namespace app\modules\site\components\behaviors;
 
 
 use app\models\tool\Debug;
+use GuzzleHttp\Psr7\CachingStream;
 use mohorev\file\UploadBehavior;
 use Tinify\Tinify;
 use yii\base\InvalidArgumentException;
@@ -16,13 +17,11 @@ class ImageUploadMinify extends UploadBehavior
     {
         if ($this->file instanceof UploadedFile) {
 
-
             $path = $this->getUploadPath($this->attribute);
 
+            // re-save image by path ?
             $compress =\Yii::$app->imageCompress;
             $compress->applyImage($this->file);
-
-            exit();
 
             if (is_string($path) && FileHelper::createDirectory(dirname($path))) {
                 $this->save($this->file, $path);
