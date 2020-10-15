f161d6a3beeda9585879cfec56a23e2321aeade2
small_jedi <popugau@gmail.com>
2020-10-15 15:44:46 +0700

    update

--- application/modules/content/widgets/informers_slider/views/default.php
+++ application/modules/content/widgets/informers_slider/views/default.php
@@ -12,27 +12,28 @@ use app\modules\catalog\models\entity\ProductPropertiesValues;
         <a class="page-title__link" href="<?= Url::to(['/catalog/']) ?>">Весь каталог</a>
     </div>
     <div class="category-slider-outter">
-    <div class="swiper-container category-slider-container">
-        <div class="swiper-wrapper category-slider-wrapper">
-			<?php foreach ($providers->all() as $provider): ?>
-                <div class="swiper-slide category-slider__slide">
-                    <div class="category-slider__info">
-                        <div class="category-slider__title">
-                            <a class="category-slider__link" href="<?= $provider->link; ?>"><?= $provider->name; ?></a>
+        <div class="swiper-container category-slider-container">
+            <div class="swiper-wrapper category-slider-wrapper">
+                <?php foreach ($providers->all() as $provider): ?>
+                    <div class="swiper-slide category-slider__slide">
+                        <div class="category-slider__info">
+                            <div class="category-slider__title">
+                                <a class="category-slider__link" href="<?= $provider->link; ?>"><?= $provider->name; ?></a>
+                            </div>
+                            <div class="category-slider__label"><?= ProductPropertiesValues::find()->where(['value' => $provider->id])->count('product_id'); ?> позиций</div>
                         </div>
-                        <div class="category-slider__label"><?= ProductPropertiesValues::find()->where(['value' => $provider->id])->count('product_id'); ?> позиций</div>
+                        <div class="category-slider__icon">
+                            <img src="/upload/<?= $provider->image; ?>" alt="<?= $provider->name; ?>" title="<?= $provider->name; ?>">
+                        </div>
                     </div>
-                    <div class="category-slider__icon">
-                        <img src="/upload/<?= $provider->image; ?>" alt="<?= $provider->name; ?>" title="<?= $provider->name; ?>">
-                    </div>
-                </div>
-			<?php endforeach; ?>
+                <?php endforeach; ?>
 
-        </div>
-        <div class="slider-swiper-button-next category-slider-control"><img src="/upload/images/arrow-right-black.svg">
-        </div>
-        <div class="slider-swiper-button-prev category-slider-control"><img src="/upload/images/arrow-left-black.svg">
+            </div>
+            <div class="slider-swiper-button-next category-slider-control"><img src="/upload/images/arrow-right-black.svg">
+            </div>
+            <div class="slider-swiper-button-prev category-slider-control"><img src="/upload/images/arrow-left-black.svg">
+            </div>
         </div>
     </div>
-	<?php $this->endCache(); ?>
+    <?php $this->endCache(); ?>
 <?php endif; ?>
