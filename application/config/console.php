46ca76236a5c2188ccf310ff497eab2949361479
small_jedi <popugau@gmail.com>
2020-10-20 11:22:02 +0700

    update

--- application/config/console.php
+++ application/config/console.php
@@ -11,6 +11,7 @@ $config = [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm' => '@vendor/npm-asset',
+        '@webroot' => dirname(dirname(__FILE__)) . '/web',
     ],
     'components' => [
         'cache' => [
