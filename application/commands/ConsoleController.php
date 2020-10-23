15503a57607b0e06d74c9ebb3e124befa7662733
small_jedi <popugau@gmail.com>
2020-10-23 12:40:30 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -2,28 +2,62 @@
 
 namespace app\commands;
 
+use app\models\tool\Debug;
 use app\modules\catalog\models\entity\Product;
 use app\modules\catalog\models\entity\ProductPropertiesValues;
+use app\modules\media\models\entity\Media;
 use app\modules\vendors\models\entity\Vendor;
 use yii\console\Controller;
+use yii\helpers\Json;
 
 class ConsoleController extends Controller
 {
     public function actionRun()
     {
-        $products = Product::find()->where(['vendor_id' => Vendor::VENDOR_ID_FORZA])->all();
+
+        $products = Product::find()->all();
+
         foreach ($products as $product) {
-            $product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
+            $path = \Yii::getAlias("@webroot/upload/$product->image");
+            if (is_file($path)) {
+                $cdnResponse = \Yii::$app->CDN->uploadImage($path);
 
-            $product->discount_price = null;
+                if (is_array($cdnResponse)) {
+                    $media = new Media();
+                    $media->json_cdn_data = Json::encode($cdnResponse);
+                    $media->location = Media::LOCATION_CDN;
+                    $media->name = $product->image;
 
-            if ($product->validate()) {
-                if ($product->update()) {
-                    echo $product->code . "=" . $product->name;
+                    if ($media->validate()) {
+                        Debug::p($media->getErrors());
+                        return false;
+                    }
+
+                    if (!$media->save()) {
+                        return false;
+                    }
+                    echo $product->image;
                     echo PHP_EOL;
                 }
+
+
+                echo "finish!!!";
             }
         }
+
+//        $products = Product::find()->where(['vendor_id' => Vendor::VENDOR_ID_FORZA])->all();
+//        foreach ($products as $product) {
+//            $product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
+//
+//            $product->discount_price = null;
+//
+//            if ($product->validate()) {
+//                if ($product->update()) {
+//                    echo $product->code . "=" . $product->name;
+//                    echo PHP_EOL;
+//                }
+//            }
+//        }
     }
 
     public function actionClearCache()
