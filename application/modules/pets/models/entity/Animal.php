3bb346aa97af3a048a13393b9cfbe47e407ee431
small_jedi <info@kotofey.store>
2020-10-25 22:55:50 +0700

    update

--- application/modules/pets/models/entity/Animal.php
+++ application/modules/pets/models/entity/Animal.php
@@ -5,24 +5,21 @@ namespace app\modules\pets\models\entity;
 use Yii;
 
 /**
- * This is the model class for table "pets".
+ * This is the model class for table "animal".
  *
  * @property int $id
- * @property int $user_id
- * @property string $name
- * @property int $animal_id
- * @property string|null $birthday
+ * @property string|null $name
  * @property int|null $created_at
  * @property int|null $updated_at
  */
-class Pets extends \yii\db\ActiveRecord
+class Animal extends \yii\db\ActiveRecord
 {
     /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
-        return 'pets';
+        return 'animal';
     }
 
     /**
@@ -31,9 +28,8 @@ class Pets extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['user_id', 'name', 'animal_id'], 'required'],
-            [['user_id', 'animal_id', 'created_at', 'updated_at'], 'integer'],
-            [['name', 'birthday'], 'string', 'max' => 255],
+            [['created_at', 'updated_at'], 'integer'],
+            [['name'], 'string', 'max' => 255],
         ];
     }
 
@@ -44,10 +40,7 @@ class Pets extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'user_id' => 'User ID',
             'name' => 'Name',
-            'animal_id' => 'Animal ID',
-            'birthday' => 'Birthday',
             'created_at' => 'Created At',
             'updated_at' => 'Updated At',
         ];
