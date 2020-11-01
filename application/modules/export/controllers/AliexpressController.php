0238e0658e1995f67f1c20418ebed8678d22e564
small_jedi <info@kotofey.store>
2020-11-01 13:46:05 +0700

    update

--- application/modules/export/controllers/AliexpressController.php
+++ application/modules/export/controllers/AliexpressController.php
@@ -6,6 +6,7 @@ namespace app\modules\export\controllers;
 use app\modules\catalog\models\entity\Category;
 use app\modules\catalog\models\entity\Product;
 use app\modules\catalog\models\entity\ProductPropertiesValues;
+use app\modules\site\models\tools\Debug;
 use yii\web\Controller;
 
 class AliexpressController extends Controller
@@ -13,8 +14,16 @@ class AliexpressController extends Controller
     public function actionIndex()
     {
         $categories = Category::find()->all();
-        $offers = Product::find()->where(['status_id' => Product::STATUS_ACTIVE])->andWhere(['is_ali' => true])->all();
+        $offers = Product::find()
+            ->select(['product.id', 'product.name', 'product.price', 'product.purchase', 'product.media_id', 'product.status_id', 'product.is_ali'])
+            ->rightJoin('product_properties_values as ppv', 'ppv.product_id = product.id')
+            ->where(['product.status_id' => Product::STATUS_ACTIVE])
+            ->where(['product.id' => 423])
+            ->andWhere(['product.is_ali' => 1])
+            ->andWhere(['in', 'ppv.property_id', [16, 17, 18]])
+            ->andWhere(['is not', 'ppv.value', null]);
 
+        $offers = $offers->all();
         \Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
         \Yii::$app->response->headers->add('Content-Type', 'text/xml');
 
