4c2371d7122e6b546be2ed5e1877401e2e9c93fd
small_jedi <info@kotofey.store>
2020-11-02 23:47:35 +0700

    update

--- application/config/db.php
+++ application/config/db.php
@@ -2,7 +2,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=kotofey_store',
-    'username' => 'kotofey',
+    'username' => 'root',
     'password' => '123qweR%',
     'charset' => 'utf8',
 
