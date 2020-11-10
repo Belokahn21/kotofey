902fb20a0be8abf2400af6f47a1eb9c3d58d9a94
small_jedi <popugau@gmail.com>
2020-11-10 17:39:59 +0700

    update

--- application/modules/content/models/helpers/SlidersImagesHelper.php
+++ application/modules/content/models/helpers/SlidersImagesHelper.php
@@ -4,12 +4,15 @@ namespace app\modules\content\models\helpers;
 
 
 use app\modules\content\models\entity\SlidersImages;
+use app\modules\site\models\tools\Debug;
 
 class SlidersImagesHelper
 {
     public static function getImageUrl(SlidersImages $model, $options = [])
     {
-        return '/images/not-image.png';
+        if (Debug::isPageSpeed()) {
+            return '/images/not-image.png';
+        }
 
         if ($model->media) {
             return \Yii::$app->CDN->resizeImage($model->media->cdnData['public_id'], $options);
