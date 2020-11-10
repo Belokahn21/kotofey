0ab61a8ff1e96b8d09b2025058b9ea0bc28f2d6c
small_jedi <popugau@gmail.com>
2020-11-10 17:31:58 +0700

    update

--- application/modules/content/widgets/slider/SliderWidget.php
+++ application/modules/content/widgets/slider/SliderWidget.php
@@ -7,39 +7,36 @@ use yii\base\Widget;
 
 class SliderWidget extends Widget
 {
-	public $slider_id;
-	public $view = 'default';
-	public $time_cache;
+    public $slider_id;
+    public $view = 'default';
+    public $time_cache;
 
-	public function run()
-	{
-		$images = [];
-		if (!empty($this->slider_id)) {
-			$unix_now = time();
-			$cache = \Yii::$app->cache;
-			$key = SliderWidget::className() . $this->slider_id;
-			$this->time_cache = 3600 * 60 * 24;
+    public function run()
+    {
+        if (empty($this->slider_id)) return;
 
-			$images = $cache->getOrSet($key, function () use ($unix_now) {
-				$images = SlidersImages::find()->orderBy(['created_at' => SORT_DESC])->where(['active' => 1, 'slider_id' => $this->slider_id])->andWhere([
-					'or',
-					'start_at = :default and end_at = :default',
-					'start_at is null and end_at is null',
-					'start_at < :now and end_at > :now'
-				])
-					->addParams([
-						":is_active" => 1,
-						":slider_id" => $this->slider_id,
-						":now" => $unix_now,
-						":default" => 0,
-					]);
+        $unix_now = time();
+        $cache = \Yii::$app->cache;
+        $key = 'index-slider';
+        $this->time_cache = 3600 * 60 * 24;
 
-				return $images->all();
-			}, $this->time_cache);
-		}
+        $images = $cache->getOrSet($key, function () use ($unix_now) {
+            return SlidersImages::find()->orderBy(['created_at' => SORT_DESC])->where(['active' => 1, 'slider_id' => $this->slider_id])->andWhere([
+                'or',
+                'start_at = :default and end_at = :default',
+                'start_at is null and end_at is null',
+                'start_at < :now and end_at > :now'
+            ])
+                ->addParams([
+                    ":is_active" => 1,
+                    ":slider_id" => $this->slider_id,
+                    ":now" => $unix_now,
+                    ":default" => 0,
+                ])->all();
+        }, $this->time_cache);
 
-		return $this->render($this->view, [
-			'images' => $images,
-		]);
-	}
+        return $this->render($this->view, [
+            'images' => $images,
+        ]);
+    }
 }
--- application/modules/content/widgets/slider/views/default.php
+++ application/modules/content/widgets/slider/views/default.php
@@ -9,17 +9,16 @@ use app\modules\content\models\helpers\SlidersImagesHelper;
 
 ?>
 <?php if ($images): ?>
-    <?php if ($this->beginCache('index-slider_' . date("d.m.y"), ['duration' => 3600 * 24 * 7])): ?>
+    <?php if ($this->beginCache('index-slider-html', ['duration' => 3600 * 24 * 7])): ?>
         <div class="swiper-container slider-container">
             <div class="swiper-wrapper slider-wrapper">
                 <?php foreach ($images as $image): ?>
                     <div class="swiper-slide slider__slide">
-                        <?php /*
-                        <div class="slider__info"><p class="slider__h-1">BOSH GBH 240 (2.7 Дж)</p>
-                            <p class="slider__h-2">Купи Bosh<br>и е... перфорируй!</p>
-                            <p class="slider__h-3">7 935 P</p>
-                        </div>*/ ?>
-                        <?= Html::img(SlidersImagesHelper::getImageUrl($image), ['class' => 'slider__image', 'alt' => $image->text, 'title' => $image->text]) ?>
+                        <?php if (!\app\modules\site\models\tools\Debug::isPageSpeed()): ?>
+                            <?= Html::img(SlidersImagesHelper::getImageUrl($image), ['class' => 'slider__image', 'alt' => $image->text, 'title' => $image->text]) ?>
+                        <?php else: ?>
+                            <?= Html::img('/images/not-image.png', ['class' => 'slider__image', 'alt' => $image->text, 'title' => $image->text]) ?>
+                        <?php endif; ?>
                     </div>
                 <?php endforeach; ?>
             </div>
