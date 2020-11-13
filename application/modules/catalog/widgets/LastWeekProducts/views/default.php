f3ad6937bfc3b95732e6b5e038345cbdb7ee266d
small_jedi <info@kotofey.store>
2020-11-14 01:06:04 +0700

    update

--- application/modules/catalog/widgets/LastWeekProducts/views/default.php
+++ application/modules/catalog/widgets/LastWeekProducts/views/default.php
@@ -16,47 +16,49 @@ use app\modules\catalog\widgets\preview_properties\PreviewPropertiesWidget;
 ?>
 
 <?php if ($models): ?>
-    <div class="page-title__group">
-        <h2 class="page-title">Новинки недели</h2>
-        <a class="page-title__link" href="/catalog/">Все предложения</a>
-    </div>
-    <div class="swiper-container vitrine-container">
-        <div class="swiper-wrapper vitrine-wrapper">
-            <?php foreach ($models as $model): ?>
-                <div class="swiper-slide vitrine__slide">
-                    <?php if ($model->discount_price): ?>
-                        <div class="discount">- <?= ProductHelper::getPercent($model); ?>%</div>
-                    <?php endif; ?>
-                    <img class="vitrine__image swiper-lazy" data-src="<?= ProductHelper::getImageUrl($model, false, ['width' => 256, 'height' => 300, 'crop' => 'fit']); ?>" alt="<?= $model->name; ?>" title="<?= $model->image; ?>">
-                    <div class="swiper-lazy-preloader"></div>
-                    <div class="vitrine__title">
-                        <a class="vitrine__link" href="<?= $model->detail; ?>"><?= $model->name; ?></a>
-                    </div>
-                    <div class="vitrine__properties">
-                        <?= PreviewPropertiesWidget::widget([
-                            'product' => $model
-                        ]); ?>
-                    </div>
-                    <div class="vitrine__price">
-                        <?php if ($model->discount_price): ?>
-                            <span class="amount-old"><?= Price::format($model->price); ?></span>
-                            <span class="amount"><?= Price::format($model->discount_price); ?></span>
-                        <?php else: ?>
-                            <span class="amount"><?= Price::format($model->price); ?></span>
-                        <?php endif; ?>
-                        <span class="rate"><?= Currency::getInstance()->show(); ?> / шт</span>
-                    </div>
-                    <?= AddBasketWidget::widget([
-                        'product_id' => $model->id,
-                        'price' => $model->price,
-                        'showInfo' => false,
-                        'showOneClick' => false,
-                        'showControl' => false,
-                        'showButton' => true,
-                    ]) ?>
-                </div>
-            <?php endforeach; ?>
+    <?php if ($this->beginCache('last-week-items-widget', ['duration' => 3600 * 24 * 7])): ?>
+        <div class="page-title__group">
+            <h2 class="page-title">Новинки недели</h2>
+            <a class="page-title__link" href="/catalog/">Все предложения</a>
         </div>
-        <div class="swiper-pagination"></div>
-    </div>
+        <div class="swiper-container vitrine-container">
+            <div class="swiper-wrapper vitrine-wrapper">
+                <?php foreach ($models as $model): ?>
+                    <div class="swiper-slide vitrine__slide">
+                        <?php if ($model->discount_price): ?>
+                            <div class="discount">- <?= ProductHelper::getPercent($model); ?>%</div>
+                        <?php endif; ?>
+                        <img class="vitrine__image swiper-lazy" data-src="<?= ProductHelper::getImageUrl($model, false, ['width' => 256, 'height' => 300, 'crop' => 'fit']); ?>" alt="<?= $model->name; ?>" title="<?= $model->image; ?>">
+                        <div class="swiper-lazy-preloader"></div>
+                        <div class="vitrine__title">
+                            <a class="vitrine__link" href="<?= $model->detail; ?>"><?= $model->name; ?></a>
+                        </div>
+                        <div class="vitrine__properties">
+                            <?= PreviewPropertiesWidget::widget([
+                                'product' => $model
+                            ]); ?>
+                        </div>
+                        <div class="vitrine__price">
+                            <?php if ($model->discount_price): ?>
+                                <span class="amount-old"><?= Price::format($model->price); ?></span>
+                                <span class="amount"><?= Price::format($model->discount_price); ?></span>
+                            <?php else: ?>
+                                <span class="amount"><?= Price::format($model->price); ?></span>
+                            <?php endif; ?>
+                            <span class="rate"><?= Currency::getInstance()->show(); ?> / шт</span>
+                        </div>
+                        <?= AddBasketWidget::widget([
+                            'product_id' => $model->id,
+                            'price' => $model->price,
+                            'showInfo' => false,
+                            'showOneClick' => false,
+                            'showControl' => false,
+                            'showButton' => true,
+                        ]) ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+        <?php $this->endCache(); endif; ?>
 <?php endif; ?>
