719d17cc78afdd0faad1fc5697d09840a88f3206
small_jedi <popugau@gmail.com>
2020-11-16 14:32:11 +0700

    update

--- application/modules/export/views/aliexpress/index.php
+++ application/modules/export/views/aliexpress/index.php
@@ -14,7 +14,7 @@ $module = Yii::$app->getModule('export');
  * @var $categories \app\modules\catalog\models\entity\Category[]
  */
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
+<? xml version = "1.0" encoding = "UTF-8"?>
 <yml_catalog date="<?= date('Y-m-d H:i'); ?>">
     <shop>
         <name><?= $module->exportOrganizationName; ?></name>
@@ -61,9 +61,9 @@ $module = Yii::$app->getModule('export');
                         <currencyId>RUB</currencyId>
                         <categoryId><?= $offer->category_id; ?></categoryId>
                         <picture><?= ProductHelper::getImageUrl($offer); ?></picture>
-                        <name><?= htmlspecialchars(strip_tags($offer->name)); ?></name>
+                        <name><?= htmlspecialchars(strip_tags(\yii\helpers\StringHelper::truncate($offer->name, 128))); ?></name>
                         <?php if (!empty($offer->description)): ?>
-                            <description><?= htmlspecialchars(strip_tags(\yii\helpers\StringHelper::truncate($offer->description, 128))); ?></description>
+                            <description><?= htmlspecialchars(strip_tags($offer->description)); ?></description>
                         <?php endif; ?>
                         <pickup>false</pickup>
                         <store>false</store>
