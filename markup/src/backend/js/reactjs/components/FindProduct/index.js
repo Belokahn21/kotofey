caf5747e2a4200ea867aa17f0f418b56ec282800
small_jedi <popugau@gmail.com>
2020-12-24 18:30:59 +0700

    update

--- markup/src/backend/js/reactjs/components/FindProduct/index.js
+++ markup/src/backend/js/reactjs/components/FindProduct/index.js
@@ -3,18 +3,22 @@ import ReactDom from "react-dom";
 import FindProductForm from './FindProductForm';
 
 class Index extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        let options = JSON.parse(props.options);
+
+        this.modalId = options.modal;
     }
 
 
     render() {
         return <div>
-            <div className="modal fade" id="' . $modalId . '" tabIndex="-1" role="dialog" aria-labelledby="' . $modalId . 'Label" aria-hidden="true">
+            <div className="modal fade" id={this.modalId} tabIndex="-1" role="dialog" aria-labelledby={this.modalId + 'Label'} aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="' . $modalId . 'Label">Найти товар</h5>
+                            <h5 className="modal-title" id={this.modalId + 'Label'}>Найти товар</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
@@ -38,6 +42,7 @@ const findProduct = document.querySelectorAll('.find-product');
 
 if (findProduct) {
     findProduct.forEach(el => {
-        ReactDom.render(<Index/>, el);
+        let options = findProduct.attribute('data-options');
+        ReactDom.render(<Index options={options}/>, el);
     });
 }
