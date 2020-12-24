5565226aad431720ae87ed3d3ee3447809151e8c
small_jedi <popugau@gmail.com>
2020-12-24 16:51:46 +0700

    update

--- application/modules/catalog/views/product-backend/_form.php
+++ application/modules/catalog/views/product-backend/_form.php
@@ -131,7 +131,7 @@ use app\modules\media\models\entity\Media;
         <div class="row">
             <div class="col-sm-6">
                 <div class="form-image single">
-                    <?php if ($model->media or $model->image): ?>
+                    <?php if ($model->media): ?>
                         <?php if ($model->media->location == Media::LOCATION_SERVER): ?>
                             <a target="_blank" href="<?= ProductHelper::getImageUrl($model) ?>">
                                 <img src="<?= ProductHelper::getImageUrl($model) ?>" width="150" title="<?= $model->name; ?>" alt="<?= $model->name; ?>">
