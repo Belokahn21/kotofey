c10fc90e2b1753cb3aa5769f09d27c8805c85213
small_jedi <popugau@gmail.com>
2020-12-29 13:03:27 +0700

    update

--- markup/src/backend/js/ecmascript6/block/CleanOrderPhone.js
+++ markup/src/backend/js/ecmascript6/block/CleanOrderPhone.js
@@ -1,27 +1,30 @@
 class CleanOrderPhone {
     constructor() {
         this.el = document.querySelector('.clean-phone');
+        this.replacer = [];
 
         if (!this.el) return false;
 
+        this.initReplaceData();
         this.initEvents();
-
     }
 
     initEvents() {
         this.el.onchange = this.handleInput.bind(this);
         this.el.onkeyup = this.handleInput.bind(this);
+    }
 
+    initReplaceData() {
+        this.replacer['+7'] = '8';
+        this.replacer[')'] = '';
+        this.replacer['('] = '';
+        this.replacer[' '] = '';
+        this.replacer['-'] = '';
     }
 
     handleInput(event) {
         let el = event.target;
-
-        el.value = el.value.replace('+7', '8');
-        el.value = el.value.replace(')', '');
-        el.value = el.value.replace('(', '');
-        el.value = el.value.replace(' ', '');
-        el.value = el.value.replace('-', '');
+        for (let key in this.replacer) el.value = el.value.replace(key, this.replacer[key]);
     }
 }
 
