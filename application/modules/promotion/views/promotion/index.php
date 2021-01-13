3f01533e76de1d613b79934aa652be8180a03c91
small_jedi <popugau@gmail.com>
2021-01-13 10:46:23 +0700

    update

--- application/modules/promotion/views/promotion/index.php
+++ application/modules/promotion/views/promotion/index.php
@@ -21,15 +21,20 @@ $this->params['breadcrumbs'][] = ['label' => 'Скидки и акции', 'url'
     ]); ?>
     <h1 class="page__title">Скидки и акции</h1>
     <?php if ($models): ?>
-        <?php foreach ($models as $model): ?>
-            <a href="<?= PromotionHelper::getDetailUrl($model); ?>">
-                <?php if ($model->media): ?>
-                    <?= Html::img(\Yii::$app->CDN->resizeImage($model->media->cdnData['public_id'])) ?>
-                <?php else: ?>
-                    <?= $model->name; ?>
-                <?php endif; ?>
-            </a>
-        <?php endforeach; ?>
+        <div class="promotion-list">
+
+            <?php foreach ($models as $model): ?>
+                <a href="<?= PromotionHelper::getDetailUrl($model); ?>" class="promotion-list__item">
+                    <?php if ($model->media): ?>
+                        <?= Html::img(\Yii::$app->CDN->resizeImage($model->media->cdnData['public_id']), [
+                            'class' => 'promotion-list__image'
+                        ]) ?>
+                    <?php else: ?>
+                        <?= $model->name; ?>
+                    <?php endif; ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
     <?php else: ?>
         <p>Новых акций нету, приходите чуть позже!</p>
     <?php endif; ?>
