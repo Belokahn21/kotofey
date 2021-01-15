c224990bcebb3987754f5d17a1fc5dd6c1887429
small_jedi <popugau@gmail.com>
2021-01-15 14:10:41 +0700

    update

---
+++ application/modules/pets/views/animal-backend/_form.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $form \yii\widgets\ActiveForm
+ * @var $model \app\modules\menu\models\entity\Menu
+ */
+?>
+
+<nav>
+    <div class="nav nav-tabs" id="backendForms" role="tablist">
+        <a class="nav-item nav-link active" id="nav-main-edit-tab" data-toggle="tab" href="#nav-main-edit" role="tab" aria-controls="nav-main-edit" aria-selected="false">Главное</a>
+    </div>
+</nav>
+<div class="tab-content" id="backendFormsContent">
+    <div class="tab-pane fade show active" id="nav-main-edit" role="tabpanel" aria-labelledby="nav-main-edit">
+        <?= $form->field($model, 'name'); ?>
+    </div>
+</div>
--- application/modules/pets/views/animal-backend/index.php
+++ application/modules/pets/views/animal-backend/index.php
@@ -1,12 +1,61 @@
-<div class="pets-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
-</div>
+<?php
+
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+use app\models\tool\seo\Title;
+use yii\grid\GridView;
+use yii\helpers\Url;
+use app\modules\order\models\helpers\OrderHelper;
+use app\modules\order\widgets\group_buy\GroupBuyWidget;
+use yii\helpers\ArrayHelper;
+use app\modules\order\models\entity\OrderStatus;
+
+/* @var $model \app\modules\menu\models\entity\Menu
+ * @var $this \yii\web\View
+ */
+
+$this->title = Title::showTitle("Животные");
+?>
+    <div class="title-group">
+        <h1>Животные</h1>
+    </div>
+<?php $form = ActiveForm::begin() ?>
+<?= $this->render('_form', [
+    'model' => $model,
+    'form' => $form,
+]); ?>
+<?= Html::submitButton('Добавить', ['class' => 'btn-main']) ?>
+<?php ActiveForm::end() ?>
+    <h2 class="title">Список животных</h2>
+<?php echo GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'emptyText' => 'Животные отсутствуют',
+    'columns' => [
+        'id',
+        'name',
+        [
+            'attribute' => 'created_at',
+            'value' => function ($model) {
+                return date("d.m.Y", $model->created_at);
+            }
+        ],
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'buttons' => [
+                'view' => function ($url, $model, $key) {
+                    return Html::a('<i class="fas fa-file-alt"></i>', Url::to(["order-report", 'id' => $key]));
+                },
+                'update' => function ($url, $model, $key) {
+                    return Html::a('<i class="far fa-eye"></i>', Url::to(["update", 'id' => $key]));
+                },
+                'delete' => function ($url, $model, $key) {
+//                    if ($key) {
+//                        return Html::a('<i class="fas fa-trash-alt"></i>',
+//                            Url::to(["admin/order", 'id' => $key, 'action' => 'delete']));
+//                    }
+                }
+            ]
+        ],
+    ],
+]);
