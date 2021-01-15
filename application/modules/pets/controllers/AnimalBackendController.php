a1b28f4db3ce540e09d0826a19f01206df2c8639
small_jedi <popugau@gmail.com>
2021-01-15 13:54:42 +0700

    update

--- application/modules/pets/controllers/AnimalBackendController.php
+++ application/modules/pets/controllers/AnimalBackendController.php
@@ -2,8 +2,9 @@
 
 namespace app\modules\pets\controllers;
 
-use app\modules\pets\models\entity\Animal;
+use app\widgets\notification\Alert;
 use yii\web\Controller;
+use yii\web\HttpException;
 
 class AnimalBackendController extends Controller
 {
@@ -12,8 +13,42 @@ class AnimalBackendController extends Controller
     public function actionIndex()
     {
         $model = new $this->modelClass();
+        if (\Yii::$app->request->isPost) {
+            if ($model->load(\Yii::$app->request->post())) {
+                if ($model->validate() && $model->save()) {
+                    Alert::setSuccessNotify('Элемент успешно добавлен');
+                    return $this->refresh();
+                }
+            }
+        }
         return $this->render('index', [
             'model' => $model
         ]);
+    }
+
+    public function actionUpdate($id)
+    {
+        if (!$model = $this->modelClass::findOne($id)) throw new HttpException(404, 'Элемент не найден');
+
+        if (\Yii::$app->request->isPost) {
+            if ($model->load(\Yii::$app->request->post())) {
+                if ($model->validate() && $model->save()) {
+                    Alert::setSuccessNotify('Элемент успешно обновлен');
+                    return $this->refresh();
+                }
+            }
+        }
+        return $this->render('index', [
+            'model' => $model
+        ]);
+    }
+
+    public function actionDelete($id)
+    {
+        if (!$model = $this->modelClass::findOne($id)) throw new HttpException(404, 'Элемент не найден');
+
+        if ($model->delete()) Alert::setSuccessNotify('Элемент успешно удален');
+
+        return $this->redirect(['index']);
     }
 }
