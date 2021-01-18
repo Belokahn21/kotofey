e5d3809ef618433add8f9fc4561ca2b48499fcf6
small_jedi <popugau@gmail.com>
2021-01-18 16:42:20 +0700

    update

--- application/modules/order/views/order/view.php
+++ application/modules/order/views/order/view.php
@@ -29,64 +29,60 @@ $this->params['breadcrumbs'][] = ['label' => 'Просмотр заказа', 'u
     <h1 class="page__title">Просмотр заказа: #<?= $order->id; ?></h1>
     <div class="row" style="width: 100%;">
         <div class="col-6">
-            <table style="width:100%">
-                <tr>
-                    <td colspan="2" align="center">Информация о заказе</td>
-                </tr>
-                <tr>
-                    <td>Дата создания</td>
-                    <td><?= date('d.m.Y', $order->created_at) ?></td>
-                </tr>
+            <div class="profile-order-info">
+                <div class="profile-order-info__row">
+                    <div class="profile-order-info__key">Дата создания</div>
+                    <div class="profile-order-info__value"><?= date('d.m.Y', $order->created_at) ?></div>
+                </div>
                 <?php if (!empty($order->email)): ?>
-                    <tr>
-                        <td>Почта</td>
-                        <td><?= $order->email; ?></td>
-                    </tr>
+                    <div class="profile-order-info__row">
+                        <div class="profile-order-info__key">Почта</div>
+                        <div class="profile-order-info__value"><?= $order->email; ?></div>
+                    </div>
                 <?php endif; ?>
                 <?php if (!empty($order->phone)): ?>
-                    <tr>
-                        <td>Телефон</td>
-                        <td class="js-phone-mask"><?= $order->phone; ?></td>
-                    </tr>
+                    <div class="profile-order-info__row">
+                        <div class="profile-order-info__key">Телефон</div>
+                        <div class="profile-order-info__value js-phone-mask"><?= $order->phone; ?></div>
+                    </div>
                 <?php endif; ?>
-                <tr>
-                    <td>Сумма заказа</td>
-                    <td>
+                <div class="profile-order-info__row">
+                    <div class="profile-order-info__row">Сумма заказа</div>
+                    <div class="profile-order-info__value">
                         <?php if (OrderHelper::orderSummary($order) == OrderHelper::orderSummary($order)): ?>
                             <span><?= Price::format(OrderHelper::orderSummary($order)); ?> <?= Currency::getInstance()->show(); ?></span>
                         <?php else: ?>
                             <span style="text-decoration: line-through;"><?= Price::format(OrderHelper::orderSummary($order)); ?> <?= Currency::getInstance()->show(); ?></span>
                             <span><?= Price::format(OrderHelper::orderSummary($order)); ?> <?= Currency::getInstance()->show(); ?></span>
                         <?php endif; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Статус</td>
-                    <td><?= OrderHelper::getStatus($order); ?></td>
-                </tr>
-                <tr>
-                    <td>Оплачено</td>
-                    <td><?= ($order->is_paid ? Html::tag('span', 'Оплачено', ['class' => 'green']) : Html::tag('span', 'Не оплачено', ['class' => 'red'])); ?></td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="center">Адрес и время доставки</td>
-                </tr>
-                <tr>
-                    <td>
+                    </div>
+                </div>
+                <div class="profile-order-info__row">
+                    <div class="profile-order-info__key">Статус</div>
+                    <div class="profile-order-info__value"><?= OrderHelper::getStatus($order); ?></div>
+                </div>
+                <div class="profile-order-info__row">
+                    <div class="profile-order-info__key">Оплачено</div>
+                    <div class="profile-order-info__value"><?= ($order->is_paid ? Html::tag('span', 'Оплачено', ['class' => 'green']) : Html::tag('span', 'Не оплачено', ['class' => 'red'])); ?></div>
+                </div>
+                <div class="profile-order-info__row">
+                    <div class="profile-order-info__key">Адрес и время доставки</div>
+                    <div class="profile-order-info__value">
                         <?= $order->city ? "г. " . $order->city : null; ?>
                         <?= $order->street ? ", ул. " . $order->street : null; ?>
                         <?= $order->number_home ? ", дом. " . $order->number_home : null; ?>
                         <?= $order->entrance ? ", подъезд. " . $order->entrance : null; ?>
                         <?= $order->floor_house ? ", этаж. " . $order->floor_house : null; ?>
                         <?= $order->number_appartament ? ", кв. " . $order->number_appartament : null; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
+                    </div>
+                </div>
+                <div class="profile-order-info__row">
+                    <div class="profile-order-info__key"></div>
+                    <div class="profile-order-info__value">
                         Дата <?= $order->dateDelivery->date; ?>, время: <?= $order->dateDelivery->time; ?>
-                    </td>
-                </tr>
-            </table>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="col-6">
             <?php if ($items): ?>
