86d28144e01e87dd7dd235bc7d8f17179b969395
small_jedi <popugau@gmail.com>
2021-02-11 11:20:31 +0700

    update

--- application/modules/export/controllers/AliexpressController.php
+++ application/modules/export/controllers/AliexpressController.php
@@ -15,7 +15,7 @@ class AliexpressController extends Controller
     {
         set_time_limit(0);
         $categories = ProductCategory::find()->all();
-        $offers = Product::find()->where(['status_id' => Product::STATUS_ACTIVE]);
+        $offers = Product::find()->where(['status_id' => Product::STATUS_ACTIVE])->andWhere(['>', 'count', 0]);
 
 
         \Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
