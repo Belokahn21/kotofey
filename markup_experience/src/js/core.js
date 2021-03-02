2f620ced57292f8f4dc7b4920d6839592f7ec2f1
small_jedi <popugau@gmail.com>
2021-03-02 13:10:29 +0700

    update

--- markup_experience/src/js/core.js
+++ markup_experience/src/js/core.js
@@ -5,35 +5,39 @@ import 'lightbox2';
 import 'popper.js';
 import 'bootstrap';
 
+// class block
 import ProductCalc from './block/PoductCalc/ProductCalc';
+import Menu from './block/Menu';
+import Favorite from './block/favorite';
+import Compare from './block/compare';
+import Catalog from './block/Catalog';
+import StickFilter from './block/StickFilter';
+import RemoveBasketItem from './block/RemoveBasketItem';
+import FastCart from './block/FastCart';
+
+// no class block
 import './block/gifts';
 import './block/mobile-menu';
 import './block/basket';
 import './block/search';
-import Menu from './block/Menu';
 import './block/ui-placeholder';
 import './block/other-jquery';
 import './block/catalog-filter';
-import Favorite from './block/favorite';
-import Compare from './block/compare';
 import './block/toggleClass';
 import './block/promocode';
+
+// init
 import './include/init-sliders';
 import './include/init-range-sliders';
 import './include/init-maskedinput';
 import './include/init-datepicker';
 import './include/phone-mask';
-import Catalog from './block/Catalog';
-import StickFilter from './block/StickFilter';
-import RemoveBasketItem from './block/RemoveBasketItem';
-import FastCart from './block/FastCart';
 
 new StickFilter(new Catalog());
 new RemoveBasketItem();
 new Compare();
 new Favorite();
 new Menu();
-
 
 document.addEventListener('DOMContentLoaded', () => {
     new ProductCalc(new FastCart());
