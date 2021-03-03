96a97a91341d6cb39b93949428e7d52959f61ffd
small_jedi <popugau@gmail.com>
2021-03-03 15:02:34 +0700

    update

--- application/modules/catalog/widgets/CatalogSliders/Recomended/RecomendedWidget.php
+++ application/modules/catalog/widgets/CatalogSliders/Recomended/RecomendedWidget.php
@@ -8,8 +8,14 @@ use app\modules\catalog\models\entity\Product;
 use app\modules\catalog\models\entity\Properties;
 use app\modules\catalog\models\entity\TypeProductProperties;
 use app\modules\catalog\widgets\CatalogSliders\RenderSlider\RenderSliderWidget;
+use app\modules\site\models\tools\Debug;
 use yii\base\Widget;
 
+
+/**
+ * @var $product Product
+ * @var $property_id integer
+ */
 class RecomendedWidget extends Widget
 {
     public $view = 'default';
@@ -20,18 +26,14 @@ class RecomendedWidget extends Widget
     {
         if (empty($this->property_id)) return false;
 
+        $listIdRelatedItems = [];
+        foreach ($this->product->propsValues as $propsValue) {
+            if ($propsValue->property_id == $this->property_id) $listIdRelatedItems[] = (int)$propsValue->value;
+        }
 
-        $property = Properties::findOne($this->property_id);
-        $property_id = $this->property_id;
-
-
-        if (!$property instanceof Properties or $property->type != TypeProductProperties::TYPE_CATALOG) return false;
-
-
-        $models = \Yii::$app->cache->getOrSet('recomended:' . $this->product->id, function () use ($property_id) {
-            return Product::find()->joinWith('propsValues')->where(['properties_product_values.property_id' => $property_id])->all();
+        $models = \Yii::$app->cache->getOrSet('recomended:' . $this->product->id, function () use ($listIdRelatedItems) {
+            return $models = Product::find()->where(['in', 'id', $listIdRelatedItems])->all();
         });
-
 
         return RenderSliderWidget::widget([
             'models' => $models,
