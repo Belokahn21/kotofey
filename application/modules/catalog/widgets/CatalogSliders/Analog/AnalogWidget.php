632ccb0fe813e901958d5a65ba0a3f1fd8d90239
small_jedi <popugau@gmail.com>
2021-03-04 17:52:38 +0700

    update

--- application/modules/catalog/widgets/CatalogSliders/Analog/AnalogWidget.php
+++ application/modules/catalog/widgets/CatalogSliders/Analog/AnalogWidget.php
@@ -26,9 +26,9 @@ class AnalogWidget extends Widget
             if ($propsValue->property_id == $this->property_id) $listIdRelatedItems[] = (int)$propsValue->value;
         }
 
-        $models = \Yii::$app->cache->getOrSet('analog:' . $this->product->id, function () use ($listIdRelatedItems) {
-            return $models = Product::find()->where(['in', 'id', $listIdRelatedItems])->all();
-        });
+//        $models = \Yii::$app->cache->getOrSet('analog:' . $this->product->id, function () use ($listIdRelatedItems) {
+        $models = Product::find()->where(['in', 'id', $listIdRelatedItems])->all();
+//        });
 
 
         return RenderSliderWidget::widget([
