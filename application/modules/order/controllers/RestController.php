68ccc85876486333131681389c985d30c787af30
small_jedi <popugau@gmail.com>
2021-03-12 19:07:48 +0700

    update

--- application/modules/order/controllers/RestController.php
+++ application/modules/order/controllers/RestController.php
@@ -3,21 +3,22 @@
 
 namespace app\modules\order\controllers;
 
-
-use yii\filters\auth\HttpBasicAuth;
+use app\modules\order\models\entity\OrdersItems;
+use app\modules\order\models\entity\OrderDate;
+use app\modules\order\models\entity\Order;
 use yii\rest\Controller;
 
 class RestController extends Controller
 {
     public $modelClass = 'app\modules\order\models\entity\Order';
 
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        $behaviors['authenticator'] = HttpBasicAuth::className();
-
-        return $behaviors;
-    }
+//    public function behaviors()
+//    {
+//        $behaviors = parent::behaviors();
+//        $behaviors['authenticator'] = HttpBasicAuth::className();
+//
+//        return $behaviors;
+//    }
 
     protected function verbs()
     {
@@ -47,11 +48,57 @@ class RestController extends Controller
 
     public function actionCreate()
     {
+        $order = new $this->modelClass(['scenario' => Order::SCENARIO_CLIENT_BUY]);
+        $orderDate = new OrderDate();
+        $items = new OrdersItems();
+        $response = [
+            'status' => 200,
+        ];
+
+        if (!$order->load(\Yii::$app->request->post())) {
+            $response['status'] = 500;
+            $response['errors'] = 'Данные в модель Order не были загружены';
+            return $response;
+        }
+
+        if (!$order->validate() || !$order->save()) {
+            $response['status'] = 510;
+            $response['errors'] = $order->getErrors();
+            return $response;
+        }
+
+        $items->order_id = $order->id;
+        if (!$items->saveItems()) {
+            $response['status'] = 530;
+            $response['errors'] = $items->getErrors();
+            return $response;
+        }
+
+
+        return $response;
     }
 
     public function actionDelete($id)
     {
-        return $this->modelClass::findOne($id)->delete();
+        $model = $this->modelClass::findOne($id);
+
+        if (!$model) return array(
+            'status' => 500,
+            'error' => 'Элемент не найден'
+        );
+
+        if ($model->delete()) {
+            return [
+                'status' => 500,
+                'error' => 'Ошибка при удалении элемента',
+                'errors' => $model->getErrors()
+            ];
+        }
+
+
+        return [
+            'status' => 200
+        ];
     }
 
 
@@ -87,24 +134,24 @@ class RestController extends Controller
 //        if (!$order->load(\Yii::$app->request->post())) {
 //            $response['status'] = 500;
 //            $response['errors'] = 'Данные в модель Order не были загружены';
-//            return Json::encode($response);
+//            return $response;
 //        }
 //
 //        if (!$order->validate() || !$order->save()) {
 //            $response['status'] = 510;
 //            $response['errors'] = $order->getErrors();
-//            return Json::encode($response);
+//            return $response;
 //        }
 //
 //        $items->order_id = $order->id;
 //        if (!$items->saveItems()) {
 //            $response['status'] = 530;
 //            $response['errors'] = $items->getErrors();
-//            return Json::encode($response);
+//            return $response;
 //        }
 //
 //
-//        return Json::encode($response);
+//        return $response;
 //    }
 //
 //    public function actionAll()
