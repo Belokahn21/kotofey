c0a2a938d16a4886ad72c9bdfda0b14636caf2b2
small_jedi <popugau@gmail.com>
2021-03-18 13:14:14 +0700

    update

--- markup_experience/src/js/react/checkout/field/DateDeliveryField.js
+++ markup_experience/src/js/react/checkout/field/DateDeliveryField.js
@@ -1,7 +1,12 @@
 import React,{Component} from "react";
+
 class DateDeliveryField extends Component{
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        $(this.refs.datepicker).datepicker();
     }
 
     render() {
@@ -9,7 +14,7 @@ class DateDeliveryField extends Component{
             <label className="checkout-form__label" htmlFor="checkout-date-delivery">
                 <div className="checkout-form__label-text">Дата доставки*</div>
                 <div className="form-group field-checkout-date-delivery required">
-                    <input type="text" id="checkout-date-delivery" className="js-datepicker checkout-form__input" name="OrderDate[date]" placeholder="Дата доставки" data-min="1614546000" />
+                    <input type="text" id="checkout-date-delivery" ref="datepicker" className="checkout-form__input" name="OrderDate[date]" placeholder="Дата доставки" data-min="1614546000" />
                     <p className="help-block help-block-error"/>
                 </div>
             </label>
