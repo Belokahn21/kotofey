32f6d08e576e0e524461f020090c52dd0198304f
small_jedi <popugau@gmail.com>
2021-03-30 11:27:34 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -2,6 +2,8 @@
 
 namespace app\commands;
 
+use app\modules\catalog\models\entity\Product;
+use app\modules\vendors\models\entity\Vendor;
 use yii\console\Controller;
 
 class ConsoleController extends Controller
@@ -9,6 +11,27 @@ class ConsoleController extends Controller
     public function actionRun()
     {
 
+        $products = Product::find()->orFilterWhere(['vendor_id' => Vendor::VENDOR_ID_MARS])
+            ->orFilterWhere(['like', 'name', 'cesar'])
+            ->orFilterWhere(['like', 'name', 'pedigree'])
+            ->orFilterWhere(['like', 'name', 'dreamies'])
+            ->orFilterWhere(['like', 'name', 'chappi'])
+            ->orFilterWhere(['like', 'name', 'whiskas'])
+            ->orFilterWhere(['like', 'name', 'kitekat'])
+            ->orFilterWhere(['like', 'name', 'perfect fit'])
+            ->all();
+
+        foreach ($products as $product) {
+            $product->scenario = Product::SCENARIO_UPDATE_PRODUCT;
+            $product->status_id = Product::STATUS_DRAFT;
+            $product->vendor_id = Vendor::VENDOR_ID_MARS;
+
+
+            echo "item: " . $product->name . PHP_EOL;
+            if ($product->validate() && $product->update()) {
+                echo "Updated: " . $product->name . PHP_EOL;
+            }
+        }
     }
 
     public function actionClearCache()
