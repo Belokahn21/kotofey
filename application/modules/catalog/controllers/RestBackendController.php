35b7be37dc3c020a5e9058f0bd6e380ebc8d80b5
small_jedi <info@kotofey.store>
2021-04-04 17:59:23 +0700

    update

--- application/modules/catalog/controllers/RestBackendController.php
+++ application/modules/catalog/controllers/RestBackendController.php
@@ -2,53 +2,17 @@
 
 namespace app\modules\catalog\controllers;
 
+use yii\filters\Cors;
+use yii\rest\ActiveController;
 
-use app\modules\catalog\models\entity\Product;
-use yii\helpers\Json;
-use yii\rest\Controller;
-
-class RestBackendController extends Controller
+class RestBackendController extends ActiveController
 {
-    protected function verbs()
-    {
-        return [
-            'all' => ['GET']
-        ];
-    }
-
-    public function beforeAction($action)
-    {
-        $this->enableCsrfValidation = false;
-        return parent::beforeAction($action);
-    }
-
     public function behaviors()
     {
-        return [
-            'corsFilter' => [
-                'class' => \yii\filters\Cors::className(),
-            ],
+        $behaviors = parent::behaviors();
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className()
         ];
-    }
-
-    public function actionAll($product_id = null)
-    {
-        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-
-        $query = Product::find();
-
-        if ($product_id) $query->where(['id' => $product_id]);
-
-        if ($phrase = \Yii::$app->request->get('name')) {
-            foreach (explode(' ', $phrase) as $text_line) {
-                $query->andFilterWhere([
-                    'or',
-                    ['like', 'name', $text_line],
-                    ['like', 'feed', $text_line]
-                ]);
-            }
-        }
-
-        return Json::encode($query->all());
+        return $behaviors;
     }
 }
