ce44c50a89aa6feed8991efc9f4eb0b58ed1d1dc
small_jedi <info@kotofey.store>
2021-04-06 23:27:20 +0700

    update

--- application/modules/acquiring/models/services/fiscalisation/models/OFDApi.php
+++ application/modules/acquiring/models/services/fiscalisation/models/OFDApi.php
@@ -11,7 +11,12 @@ use yii\helpers\Json;
 class OFDApi
 {
     private $ofd_token;
+    private $module;
+
+    const MODULE_ID = 'acquiring';
+
     const URL = 'https://ferma.ofd.ru/api/kkt/cloud/';
+    const DEMO_URL = 'https://ferma-test.ofd.ru/api/kkt/cloud/';
     const ACTION_CREATE_CHECK = 'receipt';
 
     /**
@@ -19,17 +24,19 @@ class OFDApi
      */
     public function __construct()
     {
-        if (!\Yii::$app->hasModule('acquiring')) throw new \Exception('Модуль эквайринга не подключен.');
 
-        $module = \Yii::$app->getModule('acquiring');
+        $this->getModule();
 
-        if (empty($module->ofd_login) || empty($module->ofd_password)) throw new \Exception('Нет авторизационных данных для получения токена.');
+        if (empty($this->module->ofd_login) || empty($this->module->ofd_password)) throw new \Exception('Нет авторизационных данных для получения токена.');
 
-        $this->ofd_token = new OFDAuth($module->ofd_login, $module->ofd_password);
+        $this->ofd_token = new OFDAuth($this->module->ofd_login, $this->module->ofd_password);
     }
 
     public function sendCheck(Order $order, $userData = [])
     {
+
+        $items = $order->items;
+
         $params = [
             'Inn' => ModuleSettingsHelper::getValue('acquiring', 'inn'),
             'Type' => 'income',
@@ -40,8 +47,53 @@ class OFDApi
                 'Email' => $userData['email'],
                 'PaymentType' => 'VatNo',
                 'KktFA' => true,
+                'CustomUserProperty' => [
+//                    'AutomatNumber' => "123456",
+                    'BillAddress' => "Барнаул, ул. Северо-Западная, 6Б",
+                ],
+
             ],
         ];
+
+        $paramsItems = [];
+        foreach ($items as $item) {
+            $paramsItems[] = [
+                "Label" => $item->name,
+                "Price" => $item->price,
+                "Quantity" => $item->count,
+                "Amount" => $item->price * $item->count,
+                "Vat" => "Vat0",
+//                "MarkingCode" => "000559D39E7F197241424331323334",
+//                "MarkingCodeStructured" => [
+//                    "Type" => "MEDICINES",
+//                    "Gtin" => "77777777777777",
+//                    "Serial" => "RXWWWRRRRRRRR"
+//                ],
+                "PaymentMethod" => 4,
+//                "OriginCountryCode" => "398",
+//                "CustomsDeclarationNumber" => "ТаможняДала Добро №1/#15",
+                "PaymentType" => 1,
+                "PaymentAgentInfo" => [
+                    "AgentType" => "BANK_PAYMENT_AGENT",
+                    "TransferAgentPhone" => "+79000000001",
+                    "TransferAgentName" => "наименование оператора перевода",
+                    "TransferAgentAddress" => "адрес оператора перевода",
+                    "TransferAgentINN" => "1234567890",
+                    "PaymentAgentOperation" => "операция платежного агента",
+                    "PaymentAgentPhone" => "+79000000002",
+                    "ReceiverPhone" => "+79000000003",
+                    "SupplierInn" => "012345678901",
+                    "SupplierName" => "Иван Иванов",
+                    "SupplierPhone" => "+79000000004"
+                ],
+                'Cashier' => [
+                    "Name" => "Васин Константин Викторович",
+                    "Inn" => "991133557"
+                ]
+            ];
+        }
+
+        $params['Items'] = $paramsItems;
 
         $response = $this->send(self::ACTION_CREATE_CHECK, $params);
 
@@ -52,7 +104,8 @@ class OFDApi
     {
         $response = false;
         if ($curl = curl_init()) {
-            curl_setopt($curl, CURLOPT_URL, self::URL . $action);
+            curl_setopt($curl, CURLOPT_URL, self::DEMO_URL . $action);
+//            curl_setopt($curl, CURLOPT_URL, self::URL . $action);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POST, true);
 
@@ -70,5 +123,12 @@ class OFDApi
         }
 
         return $response;
+    }
+
+    public function getModule()
+    {
+        if (!\Yii::$app->hasModule(self::MODULE_ID)) throw new \Exception('Модуль эквайринга не подключен.');
+
+        $this->module = \Yii::$app->getModule(self::MODULE_ID);
     }
 }
