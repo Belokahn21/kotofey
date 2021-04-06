bf2d99a2cb106ab5ad1dcf150877ed017f5ac41d
small_jedi <popugau@gmail.com>
2021-04-06 17:31:02 +0700

    update

--- application/modules/acquiring/models/services/fiscalisation/models/OFDApi.php
+++ application/modules/acquiring/models/services/fiscalisation/models/OFDApi.php
@@ -5,15 +5,21 @@ namespace app\modules\acquiring\models\services\fiscalisation\models;
 
 class OFDApi
 {
+    const URL = 'https://ferma.ofd.ru/api/kkt/cloud/';
+
+    const ACTION_CREATE_CHECK = 'receipt';
+
     public function sendCheck()
     {
+        $params = [];
 
+        $this->send(self::ACTION_CREATE_CHECK, $params);
     }
 
-    public function send()
+    public function send($action, $data = [], $headers = [])
     {
         if ($curl = curl_init()) {
-            curl_setopt($curl, CURLOPT_URL, 'http://mysite.ru/receiver.php');
+            curl_setopt($curl, CURLOPT_URL, self::URL . $action);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POST, true);
             curl_setopt($curl, CURLOPT_POSTFIELDS, "a=4&b=7");
