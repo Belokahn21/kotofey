282526f82538d9efff4a5565d44430f6df916242
small_jedi <popugau@gmail.com>
2021-04-08 18:58:31 +0700

    update

--- application/modules/logger/controllers/LogBackendController.php
+++ application/modules/logger/controllers/LogBackendController.php
@@ -3,15 +3,33 @@
 namespace app\modules\logger\controllers;
 
 use app\modules\logger\models\entity\Logger;
+use app\modules\logger\models\search\LoggerSearchForm;
+use app\widgets\notification\Alert;
 use yii\web\Controller;
+use yii\web\HttpException;
 
 class LogBackendController extends Controller
 {
     public function actionIndex()
     {
         $model = new Logger();
+        $searchModel = new LoggerSearchForm();
+        $dataProvider = $searchModel->search(\Yii::$app->request->get());
+
+
         return $this->render('index', [
-            'model' => $model
+            'model' => $model,
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
         ]);
+    }
+
+    public function actionDelete($id)
+    {
+        if (!$model = Logger::findOne($id)) throw new HttpException(404, 'Элемент не найден');
+
+        if ($model->delete()) Alert::setSuccessNotify('Запись в логах удалена.');
+
+        return $this->redirect(['index']);
     }
 }
--- application/modules/logger/views/log-backend/index.php
+++ application/modules/logger/views/log-backend/index.php
@@ -1,12 +1,60 @@
-<div class="logger-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
-</div>
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\modules\seo\models\tools\Title;
+use app\modules\vendors\models\entity\Vendor;
+use app\modules\catalog\models\entity\Product;
+use app\modules\catalog\models\entity\ProductCategory;
+use app\models\tool\parser\providers\SibagroTrade;
+use app\modules\catalog\models\helpers\ProductHelper;
+use app\modules\catalog\widgets\stockOut\StockOutWidget;
+use app\modules\catalog\widgets\FillFromVendor\FillFromVendorWidget;
+
+/* @var $this \yii\web\View
+ * @var $model \app\modules\logger\models\entity\Logger
+ */
+
+$this->title = Title::show('Логи сайта');
+?>
+    <div class="title-group">
+        <h1>Логирование</h1>
+    </div>
+<?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'emptyText' => 'Логи отсутствуют',
+    'columns' => [
+        'id',
+        'message',
+        'status',
+        'uniqCode',
+        [
+            'attribute' => 'created_at',
+            'format' => ['date', 'dd.MM.YYYY'],
+            'options' => ['width' => '200']
+        ],
+        [
+            'attribute' => 'updated_at',
+            'format' => ['date', 'dd.MM.YYYY'],
+            'options' => ['width' => '200']
+        ],
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'buttons' => [
+                'view' => function ($url, $model, $key) {
+//                    return Html::a('<i class="far fa-copy"></i>', Url::to(["copy", 'id' => $key]));
+                },
+                'update' => function ($url, $model, $key) {
+//                    return Html::a('<i class="far fa-eye"></i>', Url::to(["update", 'id' => $key]));
+                },
+                'delete' => function ($url, $model, $key) {
+                    return Html::a('<i class="fas fa-trash-alt"></i>', Url::to(["delete", 'id' => $key]));
+                },
+            ]
+        ],
+    ],
+]); ?>
