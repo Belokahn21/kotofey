15f94b8252338156e894c6f9c3e374e1404b4f7c
small_jedi <popugau@gmail.com>
2021-04-08 13:16:26 +0700

    update

--- application/modules/site/views/site/test.php
+++ application/modules/site/views/site/test.php
@@ -4,4 +4,5 @@ $fs = new \app\modules\acquiring\models\services\fiscalisation\FiscalisationServ
 $order = \app\modules\order\models\entity\Order::findOne(560);
 
 //$fs->sendCheckClientByEmail($order, 'popugau@gmail.com');
+$fs->getCheckStatusByCheckId('2a3215cc-4be4-48f4-aef5-d95f4a7aa979 ');
 $fs->getCheckStatusByOrderId($order->id);
