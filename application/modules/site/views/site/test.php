a09bc5a6a06bcb0672b7ce4781916e6f15a0c9f3
small_jedi <popugau@gmail.com>
2021-04-09 12:40:07 +0700

    update

--- application/modules/site/views/site/test.php
+++ application/modules/site/views/site/test.php
@@ -1,6 +1,3 @@
 <?php
-//$fs = new \app\modules\acquiring\models\services\fiscalisation\FiscalisationService();
-//$order = \app\modules\order\models\entity\Order::findOne(560);
-//$fs->sendCheckClientByEmail($order, 'popugau@gmail.com');
-//$fs->getCheckStatusByCheckId('4e3451d1-bfd3-4eff-af16-2aecad751d00');
-//$fs->getCheckStatusByOrderId($order->id);
+//$order = \app\modules\order\models\entity\Order::findOne(557);
+//$ofd = \app\modules\acquiring\models\services\ofd\OFDFermaService::getInstance()->sendCheckClientByEmail($order, $order->email);
