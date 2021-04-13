473d99a3e2e1755ad7bd5757ba81dd6cd7f31251
small_jedi <info@kotofey.store>
2021-04-13 23:10:31 +0700

    update

--- application/views/layouts/include/help-desk.php
+++ application/views/layouts/include/help-desk.php
@@ -1,24 +1,28 @@
 <div class="js-help-panel help-panel">
     <div class="help-panel__header">
-        <div class="help-panel__title">Помощь по сайту</div><a class="js-close-help-panel help-panel__close" href="#"><i class="far fa-times-circle"></i></a>
+        <div class="help-panel-container">
+            <div class="help-panel__title">Помощь по сайту</div><a class="js-close-help-panel help-panel__close" href="#"><i class="far fa-times-circle"></i></a>
+        </div>
     </div>
-    <div class="accordion help-panel-list" id="help-panel-accordion">
+    <div class="accordion help-panel-container" id="help-panel-accordion">
         <div class="help-panel-list">
-            <div class="help-panel-list-header" id="headingOne"><a class="collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Как посмотреть историю заказов?</a></div>
-            <div class="collapse" id="collapseOne" aria-labelledby="headingOne" data-parent="#help-panel-accordion">
-                <div class="help-panel-list-body">Пункт в разработке</div>
+            <div class="help-panel-list__item">
+                <div class="help-panel-list-header" id="headingOne"><a class="collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><span class="help-panel-list__icon"><i class="fas fa-paw" aria-hidden="true"></i></span><span>Как посмотреть историю заказов?</span></a></div>
+                <div class="collapse" id="collapseOne" aria-labelledby="headingOne" data-parent="#help-panel-accordion">
+                    <div class="help-panel-list-body">Пункт в разработке</div>
+                </div>
             </div>
-        </div>
-        <div class="help-panel-list">
-            <div class="help-panel-list-header" id="headingTwo"><a class="collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Сколько стоит доставка?</a></div>
-            <div class="collapse" id="collapseTwo" aria-labelledby="headingTwo" data-parent="#help-panel-accordion">
-                <div class="help-panel-list-body">Пункт в разработке</div>
+            <div class="help-panel-list__item">
+                <div class="help-panel-list-header" id="headingTwo"><a class="collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><span class="help-panel-list__icon"><i class="fas fa-paw" aria-hidden="true"></i></span><span>Сколько стоит доставка?</span></a></div>
+                <div class="collapse" id="collapseTwo" aria-labelledby="headingTwo" data-parent="#help-panel-accordion">
+                    <div class="help-panel-list-body">Пункт в разработке</div>
+                </div>
             </div>
-        </div>
-        <div class="help-panel-list">
-            <div class="help-panel-list-header" id="headingThree"><a class="collapsed" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Collapsible Group Item #3</a></div>
-            <div class="collapse" id="collapseThree" aria-labelledby="headingThree" data-parent="#help-panel-accordion">
-                <div class="help-panel-list-body">Пункт в разработке</div>
+            <div class="help-panel-list__item">
+                <div class="help-panel-list-header" id="headingThree"><a class="collapsed" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"><span class="help-panel-list__icon"><i class="fas fa-paw" aria-hidden="true"></i></span><span>Collapsible Group Item #3</span></a></div>
+                <div class="collapse" id="collapseThree" aria-labelledby="headingThree" data-parent="#help-panel-accordion">
+                    <div class="help-panel-list-body">Пункт в разработке</div>
+                </div>
             </div>
         </div>
     </div>
