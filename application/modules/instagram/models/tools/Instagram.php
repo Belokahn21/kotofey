072db43c610bbbdfdf12005f821b4b0ddb94eb7f
small_jedi <popugau@gmail.com>
2021-04-15 10:52:02 +0700

    update

--- application/modules/instagram/models/tools/Instagram.php
+++ application/modules/instagram/models/tools/Instagram.php
@@ -3,19 +3,22 @@
 namespace app\modules\instagram\models\tools;
 
 
+use app\modules\instagram\Module;
+
 class Instagram
 {
-	const TOKEN = "IGQVJXV0JuNzR5RlV4elAxTnhRTFNuczRfQ0hFTnJraWh6d3FBdmlJakozS3VqZAHNLZATVIazlJSjJlTTRrR05SR1o3U3Q3bll1RnJfNFNIZA3FVVHpSUTFhbFJoeGpuTzVWbXU2UFRCdG9fdnNtdU9XQQZDZD";
+    public static function getData()
+    {
+        /* @var $module Module */
+        $module = \Yii::$app->getModule('instagram');
+        $url = "https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp,thumbnail_url,permalink&access_token=" . $module->instagram_token;
 
-	public static function getData()
-	{
-		$url = "https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp,thumbnail_url,permalink&access_token=" . self::TOKEN;
-		$instagramCnct = curl_init(); // инициализация cURL подключения
-		curl_setopt($instagramCnct, CURLOPT_URL, $url); // адрес запроса
-		curl_setopt($instagramCnct, CURLOPT_RETURNTRANSFER, 1); // просим вернуть результат
-		$media = json_decode(curl_exec($instagramCnct)); // получаем и декодируем данные из JSON
-		curl_close($instagramCnct); // закрываем соединение
+        $instagramCnct = curl_init(); // инициализация cURL подключения
+        curl_setopt($instagramCnct, CURLOPT_URL, $url); // адрес запроса
+        curl_setopt($instagramCnct, CURLOPT_RETURNTRANSFER, 1); // просим вернуть результат
+        $media = json_decode(curl_exec($instagramCnct)); // получаем и декодируем данные из JSON
+        curl_close($instagramCnct); // закрываем соединение
 
-		return $media;
-	}
+        return $media;
+    }
 }
