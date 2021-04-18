1a8b9932b511d1221ab72fc6e66a9a105766b8b9
small_jedi <info@kotofey.store>
2021-04-18 15:31:31 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -10,36 +10,6 @@ class ConsoleController extends Controller
 {
     public function actionRun()
     {
-        $bank_olded = [];
-        foreach (['inspector', 'бравекто', 'ошейник от блох'] as $phrase) {
-            $products = Product::find()->andWhere(['not in', 'id', $bank_olded]);
-
-            foreach (explode(' ', $phrase) as $text_line) {
-                $products->andFilterWhere([
-                    'or',
-                    ['like', 'name', $text_line],
-                    ['like', 'feed', $text_line]
-                ]);
-            }
-
-            $products = $products->all();
-
-
-            foreach ($products as $product) {
-
-                $mech = new PromotionProductMechanics();
-                $mech->product_id = $product->id;
-                $mech->promotion_mechanic_id = 1;
-                $mech->amount = 15;
-                $mech->discountRule = 2;
-                $mech->promotion_id = 31;
-
-                if ($mech->validate() && $mech->save()) {
-                    $bank_olded[] = $product->id;
-                    echo $product->name . PHP_EOL;
-                }
-            }
-        }
     }
 
     public function actionClearCache()
