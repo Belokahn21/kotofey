4fe8cae8af329188f58dea100e3a9950febeb080
small_jedi <popugau@gmail.com>
2021-04-19 18:05:39 +0700

    update

--- application/modules/cdn/models/components/CloudinaryComponent.php
+++ application/modules/cdn/models/components/CloudinaryComponent.php
@@ -3,7 +3,8 @@
 
 namespace app\modules\cdn\models\components;
 
-use Cloudinary\Api\Admin\AdminApi;
+
+use Cloudinary\Api;
 
 class CloudinaryComponent
 {
@@ -11,13 +12,14 @@ class CloudinaryComponent
 
     public function __construct()
     {
-        $this->_cdn = new AdminApi([
-            'cloud' => [
-                "cloud_name" => "kotofey-store",
-                "api_key" => "313768283447262",
-                "api_secret" => "Wm28QI4nQIolSV1J7Hd0hArxuzM",
-            ],
+        \Cloudinary::config([
+            "cloud_name" => "kotofey-store",
+            "api_key" => "313768283447262",
+            "api_secret" => "Wm28QI4nQIolSV1J7Hd0hArxuzM",
+            "secure" => true
         ]);
+
+        $this->_cdn = new Api();
     }
 
     public static function getInstance()
@@ -27,11 +29,11 @@ class CloudinaryComponent
 
     public function getAllResources($options = [])
     {
-        return $this->_cdn->assets($options);
+        return $this->_cdn->resources($options);
     }
 
     public function removeResource($public_id)
     {
-        return $this->_cdn->deleteAssets($public_id);
+        return $this->_cdn->delete_resources($public_id);
     }
 }
