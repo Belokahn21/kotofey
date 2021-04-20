eae1619bc40f62868f53dea56065031496bb06c9
small_jedi <popugau@gmail.com>
2021-04-20 12:49:41 +0700

    update

--- application/modules/site/views/site/test.php
+++
@@ -1,3 +0,0 @@
-<?php
-//$order = \app\modules\order\models\entity\Order::findOne(557);
-//$ofd = \app\modules\acquiring\models\services\ofd\OFDFermaService::getInstance()->sendCheckClientByEmail($order, $order->email);
