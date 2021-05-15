cd26788623efd71efb8a4d2301d4662aaac0c120
small_jedi <popugau@gmail.com>
2021-05-15 11:08:58 +0700

    update

--- application/modules/delivery/ProfileTracking/ProfileTrackingWidget.php
+++ application/modules/delivery/ProfileTracking/ProfileTrackingWidget.php
@@ -5,6 +5,7 @@ namespace app\modules\delivery\ProfileTracking;
 use app\modules\order\models\entity\Order;
 use app\modules\order\models\entity\OrderTracking;
 use app\modules\order\models\service\TrackingService;
+use app\modules\site\models\tools\Debug;
 use yii\base\Widget;
 
 /**
@@ -24,7 +25,7 @@ class ProfileTrackingWidget extends Widget
         } catch (\Exception $x) {
             echo $x->getMessage();
         }
-
+        Debug::p($tracking_info);
         if ($tracking_info === null) return false;
 
         return $this->render($this->view, [
