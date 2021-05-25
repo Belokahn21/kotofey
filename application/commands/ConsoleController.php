e14ebd906502b7e0d2b17b0568c01467064fc6b1
small_jedi <info@kotofey.store>
2021-05-25 23:37:16 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -26,7 +26,7 @@ class ConsoleController extends Controller
         }
         $products = $products->all();
         foreach ($products as $product) {
-            if (ProductPropertiesValuesHelper::savePropertyValue($product->id, 20, '231')) {
+            if (ProductPropertiesValuesHelper::savePropertyValue($product->id, 20, '232')) {
 //            if (ProductPropertiesValuesHelper::removePropertyValue($product->id, 20)) {
                 echo "ok: " . $product->name . PHP_EOL;
             }
