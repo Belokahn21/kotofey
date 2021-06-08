1bbec47ef318dc037b5f19321325d705d2c03087
small_jedi <popugau@gmail.com>
2021-06-08 19:08:06 +0700

    update

--- application/modules/order/views/operator-backend/index.php
+++ application/modules/order/views/operator-backend/index.php
@@ -26,7 +26,7 @@ $this->title = Title::show('Кабинет оператора');
             $result_summ += OrderHelper::orderSummary($order);
         }
 
-        echo sprintf('%s/<div class="green">%s</div>', Price::format($result_summ), Price::format(round($result_summ * 0.1)));
+        echo sprintf('%d/<div class="green">%d</div>', Price::format($result_summ), Price::format(round($result_summ * 0.1)));
         ?>
     </li>
 </ul>
