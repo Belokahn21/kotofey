4e38108d3aef20844771a8c89a4191d7063ecf87
small_jedi <popugau@gmail.com>
2021-06-16 17:17:17 +0700

    update

--- application/modules/logistic/views/route-backend/index.php
+++ application/modules/logistic/views/route-backend/index.php
@@ -47,7 +47,7 @@ $this->title = Title::show("Список доставок");
                         <div class="logistic-list-data__value">
                             <?= OrderHelper::getPayment($order); ?>
                             <?php if (!empty($order->odd)): ?>
-                                / <span class="red">Сдача: <?= Price::format($order->odd - OrderHelper::orderSummary($order)); ?></span>
+                                / <span class="red bold">Сдача: <?= Price::format($order->odd - OrderHelper::orderSummary($order)); ?></span>
                             <?php endif; ?>
                         </div>
                     </div>
