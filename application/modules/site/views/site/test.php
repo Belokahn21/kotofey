243d15fcfd01efcbbb5e70597f11dfd7cbb5c76d
small_jedi <popugau@gmail.com>
2021-06-16 14:42:34 +0700

    update

--- application/modules/site/views/site/test.php
+++ application/modules/site/views/site/test.php
@@ -9,14 +9,13 @@ if (!$event) return false;
 $mails = \app\modules\mailer\models\entity\MailTemplates::findAll(['event_id' => $event->id]);
 
 foreach ($mails as $mail) {
-    \app\modules\site\models\tools\Debug::p($mail);
-
-
-
     $mailer = new MailService();
     $mailer->sendEvent($event->id, [
         'EMAIL_FROM' => 'sale@kotofey.store',
         'EMAIL_TO' => 'popugau@gmail.com',
+        'ORDER_ID' => '1',
+        'STORE_ADDRESS' => 'г. Барнаул, ул. Северо-Западная, д. 6Б',
+        'STORE_TIME' => '10:00 до 19:00',
     ]);
 }
 ?>
