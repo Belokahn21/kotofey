85763416c7496502c43ed91e1e98ed209095abe1
small_jedi <popugau@gmail.com>
2021-06-18 18:29:48 +0700

    update

--- application/modules/mailer/install/migrations/m210618_112327_modify_column.php
+++ application/modules/mailer/install/migrations/m210618_112327_modify_column.php
@@ -6,7 +6,7 @@ class m210618_112327_modify_column extends Migration
 {
     public function safeUp()
     {
-        $this->execute('ALTER TABLE `mail_templates` MODIFY `text` LARGETEXT;');
+        $this->execute('ALTER TABLE `mail_templates` MODIFY `text` LONGTEXT;');
     }
 
     public function safeDown()
