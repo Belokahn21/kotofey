a2855d8d743832e3abe6fc7b991fb8225201bdaf
small_jedi <popugau@gmail.com>
2021-07-01 18:22:23 +0700

    update

--- application/modules/catalog/models/helpers/CompositionMetricsHelper.php
+++ application/modules/catalog/models/helpers/CompositionMetricsHelper.php
@@ -7,17 +7,26 @@ namespace app\modules\catalog\models\helpers;
 class CompositionMetricsHelper
 {
     const WEIGHT_PERCENT = 'percent';
-    const WEIGHT_GG = 'g';
+    const WEIGHT_GG = 'gr';
     const WEIGHT_MG = 'mg';
     const WEIGHT_KG = 'kg';
+
+    const LIQUID_GG = 'g';
+    const LIQUID_MG = 'mlg';
+    const LIQUID_L = 'l';
 
     public static function getMetrics()
     {
         return [
             self::WEIGHT_PERCENT => '%',
+
             self::WEIGHT_GG => 'Грамм',
             self::WEIGHT_MG => 'Милиграмм',
             self::WEIGHT_KG => 'Килограмм',
+
+            self::LIQUID_GG => 'Гамм',
+            self::LIQUID_MG => 'Милиграмм',
+            self::LIQUID_L => 'Литр',
         ];
     }
 }
