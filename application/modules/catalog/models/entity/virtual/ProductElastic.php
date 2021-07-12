4f0459b6fc3b53940b1d467ca6611671d325f373
small_jedi <popugau@gmail.com>
2021-07-12 18:59:21 +0700

    update

---
+++ application/modules/catalog/models/entity/virtual/ProductElastic.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\modules\catalog\models\entity\virtual;
+
+use yii\elasticsearch\ActiveRecord;
+
+class ProductElastic extends ActiveRecord
+{
+    public function attributes()
+    {
+        return [];
+    }
+
+    public function rules()
+    {
+        return [
+            ['name', 'string']
+        ];
+    }
+
+    public static function mapping()
+    {
+        return [
+            // Типы полей: https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html#field-datatypes
+            'properties' => [
+                'name' => ['type' => 'text'],
+            ]
+        ];
+    }
+
+    public static function updateMapping()
+    {
+        $db = static::getDb();
+        $command = $db->createCommand();
+        $command->setMapping(static::index(), static::type(), static::mapping());
+    }
+
+    public static function createIndex()
+    {
+        $db = static::getDb();
+        $command = $db->createCommand();
+        $command->createIndex(static::index(), [
+            //'aliases' => [ /* ... */ ],
+            'mappings' => static::mapping(),
+            //'settings' => [ /* ... */ ],
+        ]);
+    }
+
+    public static function deleteIndex()
+    {
+        $db = static::getDb();
+        $command = $db->createCommand();
+        $command->deleteIndex(static::index(), static::type());
+    }
+}
