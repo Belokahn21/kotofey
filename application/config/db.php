276d7c6491dfdcfdeffc0fa70c5bb43a8530c8ad
small_jedi <popugau@gmail.com>
2021-07-15 11:44:21 +0700

    update

--- application/config/db.php
+++ application/config/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=kotofey',
+    'dsn' => 'mysql:host=localhost;dbname=kotofey-offers',
     'username' => 'kotofey',
     'password' => '123qweR%',
     'charset' => 'utf8',
