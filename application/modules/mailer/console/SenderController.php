8a3fcef1a10480732925493d231845cada9a487b
small_jedi <popugau@gmail.com>
2021-07-20 18:51:35 +0700

    update

--- application/modules/mailer/console/SenderController.php
+++ application/modules/mailer/console/SenderController.php
@@ -16,27 +16,27 @@ class SenderController extends Controller
     {
         $module = \Yii::$app->getModule('mailer');
 
-//        if (!$module || empty($module->remember_event_id)) return false;
+        if (!$module || empty($module->remember_event_id)) return false;
 
         $orders = Order::find()
             ->select(['id', 'email', 'phone', 'created_at'])
             ->where(['!=', 'email', ''])
             ->andWhere(['in', 'created_at', Order::find()->select('MAX(created_at)')->groupBy('email')])
             ->andWhere(['<', 'created_at', strtotime('-2 month')])
-            ->andWhere(['not in', 'email', ArrayHelper::getColumn(Subscribes::find()->where(['active' => 0])->all(), 'email')]);
+            ->andWhere(['not in', 'email', ArrayHelper::getColumn(Subscribes::find()->where(['active' => 0])->all(), 'email')])
+            ->limit(5);
         $orders = $orders->all();
 
 
         foreach ($orders as $order) {
             $take_serivce = new TakeAvailableService($order->phone);
             $promo_code = $take_serivce->getPromo();
-            Debug::p($promo_code);
 
-//            $es = new MailService();
-//            $es->sendEvent($module->remember_event_id, [
-//                'PROMO_CODE' => $promo_code,
-//                'EMAIL_TO' => $order->email,
-//            ]);
+            $es = new MailService();
+            $es->sendEvent($module->remember_event_id, [
+                'PROMO_CODE' => $promo_code,
+                'EMAIL_TO' => 'popugau@gmail.com',
+            ]);
         }
     }
 }
