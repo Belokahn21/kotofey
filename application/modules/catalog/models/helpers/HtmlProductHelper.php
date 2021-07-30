62e9918c9b3bfb980306d17a32260d81bd4d4b8f
small_jedi <popugau@gmail.com>
2021-07-30 18:26:44 +0700

    update

--- application/modules/catalog/models/helpers/HtmlProductHelper.php
+++ application/modules/catalog/models/helpers/HtmlProductHelper.php
@@ -11,8 +11,11 @@ class HtmlProductHelper
     public static function showHtmlCountBuy(Product $product)
     {
         $count = OrdersItems::find()->where(['product_id' => $product->id])->count();
-        return Html::tag('div', \Yii::t('app', 'Товар купили более  {n, plural, =0{0 раз} =1{одного раза} other{# раз}}!', array(
+        if ($count > 0) return Html::tag('div', \Yii::t('app', 'Товар купили более  {n, plural, =0{0 раз} =1{одного раза} other{# раз}}!', array(
             'n' => $count,
         )), ['class' => 'product-count-buy']);
+
+
+        return false;
     }
 }
