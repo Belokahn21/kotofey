43482ca89ce656decf4b2930e1b4a7fa2f6e8d3d
small_jedi <popugau@gmail.com>
2021-08-04 16:48:43 +0700

    update

--- application/modules/site/views/site/test.php
+++ application/modules/site/views/site/test.php
@@ -3,10 +3,7 @@ use app\modules\mailer\models\services\MailService;
 
 $sender = new MailService();
 
-$sender->sendEvent(6, [
+$sender->sendEvent(3, [
     'EMAIL_FROM' => 'sale@kotofey.store',
-    'EMAIL_TO' => 'popugau@gmail.com',
-    'LINK_SITE' => 'popugau@gmail.com',
-    'PROMOCODE' => 'popugau@gmail.com',
-    'DISCOUNT' => 'popugau@gmail.com',
+    'EMAIL_TO' => 'test-zaremjcfi@srv1.mail-tester.com',
 ]);
