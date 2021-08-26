9431c75a877ce4934a2e5757a52e97f04c0b8f78
small_jedi <popugau@gmail.com>
2021-08-26 16:19:44 +0700

    update

--- application/modules/site/views/site/test.php
+++ application/modules/site/views/site/test.php
@@ -1,9 +1,10 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 $url = 'http://ws.valta.ru:8888/uppms/ws/exchange.1cws?wsdl';
 $login = 'VasinKV_NSK';
 $password = 'aP85jU21g0';
-$id = '3d49908e-4ee1-11ea-8156-005056bf23ce';
+$id = "3d49908e-4ee1-11ea-8156-005056bf23ce";
 
 $client = new SoapClient($url, [
     'cache_wsdl' => WSDL_CACHE_NONE,
@@ -16,21 +17,12 @@ $client = new SoapClient($url, [
 
 \app\modules\site\models\tools\Debug::p($client->__getFunctions());
 
-//$params = new stdClass();
-//$params->ClientId = $id;
-$params = ['ClientId' => $id];
+$params = new stdClass();
+$params->ClientID = $id;
 
 
-//$myClass->token = new \stdClass;
-////$myClass->token->user = $username;
-////$myClass->token->password = $password;
-//
-//$myClass->projectId = 123;
-//$myClass->start = 0;
-//$myClass->count = 0;
-
-$response = $client->__soapCall('GetGoodList', $params);
-//$response = $client->GetGoodList($params);
+//$response = $client->__soapCall('GetGoodList', $params);
+$response = $client->GetGoodList($params);
 
 \app\modules\site\models\tools\Debug::p($response);
 ?>
