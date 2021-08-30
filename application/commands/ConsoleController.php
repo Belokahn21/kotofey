f8ed4813e8c4763eacbddc036600fdcb6d0c102a
small_jedi <info@kotofey.store>
2021-08-30 23:07:19 +0700

    update

--- application/commands/ConsoleController.php
+++ application/commands/ConsoleController.php
@@ -15,6 +15,25 @@ class ConsoleController extends Controller
 {
     public function actionRun()
     {
+
+        foreach ((new Breed())->getSizes() as $key => $value) {
+            $dom = new \DOMDocument();
+            $dom->loadHTML(file_get_contents(\Yii::getAlias("@app/tmp/{$key}.html")), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD | LIBXML_NOERROR | LIBXML_NOWARNING);
+
+            $xpath = new \DOMXPath($dom);
+            foreach ($xpath->query('//h5[@class="new_name"]') as $name) {
+                $name = $name->nodeValue;
+                $name = trim($name);
+
+                $breed = Breed::find()->where(['like', 'name', $name])->one();
+                if ($breed) {
+                    $breed->size = $key;
+                    if ($breed->validate() && $breed->update() !== false) {
+                        echo $breed->name . PHP_EOL;
+                    }
+                }
+            }
+        }
     }
 
     public function actionClearCache()
