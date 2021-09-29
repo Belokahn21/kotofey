04d61c5877cb1390bd42edbdd81eb56f87e93b5f
small_jedi <popugau@gmail.com>
2021-09-29 18:18:40 +0700

    update

--- application/modules/site/views/site/test.php
+++
@@ -1,9 +0,0 @@
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
-<script>
-    grecaptcha.execute('6LdFXZccAAAAAD6iFvTtCDkzZVLnqwaQnEwcFtnY').then(
-        function (token) {
-            console.log(token);
-        }
-    );
-</script>
