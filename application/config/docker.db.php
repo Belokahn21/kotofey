5aaf70093a991b56ec2c7cdc85854ecd645067c0
small_jedi <info@kotofey.store>
2021-10-28 00:34:47 +0700

    update

--- application/config/docker.db.php
+++ application/config/docker.db.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mariadb;dbname=kotofey;',
+    'dsn' => 'mysql:host=mariadb;dbname=kotofey;port=33061',
     'username' => 'kotofey',
     'password' => '123qweR%',
     'charset' => 'utf8',
