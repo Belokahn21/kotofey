cfe7ff9857bc6b80aa1c6f24189d2f1883d255c8
small_jedi <popugau@gmail.com>
2021-11-09 15:54:28 +0700

    update

--- application/modules/catalog/models/repository/ProductCategoryRepository.php
+++ application/modules/catalog/models/repository/ProductCategoryRepository.php
@@ -10,7 +10,7 @@ class ProductCategoryRepository
 {
     public static function getOne(int $id)
     {
-        return \Yii::$app->cache->getOrSet(__CLASS__ . __METHOD__, function () use ($id) {
+        return \Yii::$app->cache->getOrSet(__CLASS__ . __METHOD__ . $id, function () use ($id) {
             return ProductCategory::findOne($id);
         });
     }
