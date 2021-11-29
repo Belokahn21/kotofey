637d7abe8be825216365a90ceafe3435c9b79f48
small_jedi <popugau@gmail.com>
2021-11-29 11:20:45 +0700

    update

--- application/modules/site/views/site/test.php
+++ application/modules/site/views/site/test.php
@@ -13,22 +13,24 @@ $category_id = Yii::$app->request->post('category_id');
 <?= Html::endForm(); ?>
 
 
+<div class="ozon-react"></div>
+
 <?php
 
-if ($category_id) {
-    $ozon = new OzonApi();
-//\app\modules\site\models\tools\Debug::p($ozon->categoryTree(\yii\helpers\ArrayHelper::getValue(Yii::$app->request->post(), 'category_id', 0)));
-    $_data = $ozon->listCategoryCharacteristic([
-        $category_id
-    ]);
-
-    $attributes = ArrayHelper::getValue($_data, '0.attributes');
-
-    foreach ($attributes as $attribute) {
-        if ($attribute['is_required']) {
-            \app\modules\site\models\tools\Debug::p($attribute);
-            \app\modules\site\models\tools\Debug::p($ozon->attributeValues($attribute['id'], $category_id));
-        }
-    }
-}
+//if ($category_id) {
+//    $ozon = new OzonApi();
+////\app\modules\site\models\tools\Debug::p($ozon->categoryTree(\yii\helpers\ArrayHelper::getValue(Yii::$app->request->post(), 'category_id', 0)));
+//    $_data = $ozon->listCategoryCharacteristic([
+//        $category_id
+//    ]);
+//
+//    $attributes = ArrayHelper::getValue($_data, '0.attributes');
+//
+//    foreach ($attributes as $attribute) {
+//        if ($attribute['is_required']) {
+//            \app\modules\site\models\tools\Debug::p($attribute);
+//            \app\modules\site\models\tools\Debug::p($ozon->attributeValues($attribute['id'], $category_id));
+//        }
+//    }
+//}
 ?>
---
+++ markup/frontend/src/js/react/OzonApi/Ozon.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+export default class Ozon extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                demo api ozon
+            </div>
+        );
+    }
+}
+
