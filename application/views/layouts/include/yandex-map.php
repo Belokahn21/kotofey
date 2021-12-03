06a476cfdc65a78b2513926adf90ac70ba7f16fb
small_jedi <popugau@gmail.com>
2021-12-03 08:33:48 +0700

    update

--- application/views/layouts/include/yandex-map.php
+++ application/views/layouts/include/yandex-map.php
@@ -6,7 +6,7 @@
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
             </div>
             <div class="modal-body">
-                <script type="text/javascript" charset="utf-8" async="" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A75a2c08cf453a0309392fdcb243329f2301db73bdb97f18c8bc32cb9e14c1f93&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A75a2c08cf453a0309392fdcb243329f2301db73bdb97f18c8bc32cb9e14c1f93&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=true"></script>
             </div>
             <div class="modal-footer">
                 <button class="btn-main" type="button" data-dismiss="modal">Закрыть</button>
