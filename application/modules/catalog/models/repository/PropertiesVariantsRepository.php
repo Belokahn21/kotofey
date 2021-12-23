89b1db92de2f5d8e2b7e169ccf94aa41ac3d799a
small_jedi <popugau@gmail.com>
2021-12-23 13:12:18 +0700

    update

--- application/modules/catalog/models/repository/PropertiesVariantsRepository.php
+++ application/modules/catalog/models/repository/PropertiesVariantsRepository.php
@@ -9,7 +9,7 @@ class PropertiesVariantsRepository
     public static function variantsByPropertyId(int $property_id)
     {
         return \Yii::$app->cache->getOrSet(__CLASS__ . __METHOD__ . $property_id, function () use ($property_id) {
-            return PropertiesVariants::find()->where(['property_id' => $property_id])->orderBy(['name' => SORT_ASC])->all();
+            return PropertiesVariants::find()->asArray(true)->select(['id', 'name'])->where(['property_id' => $property_id])->orderBy(['name' => SORT_ASC])->all();
         });
     }
 }
